b42e7017ab9bd70c6f50913e1a09691361581a80
Thibaut <thibaut@me.com>
2013-12-21 07:41:04 +0000

    Update Node documentation to 0.10.24

--- lib/docs/scrapers/node.rb
+++ lib/docs/scrapers/node.rb
@@ -3,7 +3,7 @@ module Docs
     self.name = 'Node.js'
     self.slug = 'node'
     self.type = 'node'
-    self.version = '0.10.22'
+    self.version = '0.10.24'
     self.base_url = 'http://nodejs.org/api/'
 
     html_filters.push 'node/clean_html', 'node/entries', 'title'
