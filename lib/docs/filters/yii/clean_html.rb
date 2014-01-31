d092e6483974e351061215d8206a9cc357c524c9
Kirk Hansen <kirk.hansen@colorfx.net>
2014-01-31 11:03:11 -0600

    Trying to get the code to format to convention.
    Git shows code differently than my editor.

--- lib/docs/filters/yii/clean_html.rb
+++ lib/docs/filters/yii/clean_html.rb
@@ -3,14 +3,14 @@ module Docs
     class CleanHtmlFilter < Filter
       def call
 		
-		#remove meaningless content
-      	css('div').each do |node| 
+	    #remove meaningless content
+        css('div').each do |node| 
 		  if node['class'] == "layout-main-header"
-			node.remove
+		    node.remove
 		  elsif node['class'] == "layout-main-submenu"
-		    node.remove
-		  elsif node['class'] == "layout-main-shortcuts"
-		    node.remove
+            node.remove
+          elsif node['class'] == "layout-main-shortcuts"
+            node.remove
 		  elsif node['class'] == "layout-main-footer"
 			node.remove
 		  elsif node['class'] == "grid_3 alpha"
@@ -28,23 +28,23 @@ module Docs
 		  end
 		  
 		#remove javascript show links
-		css('b').each do |node|
-		  if node['class'] == "show"
-		    node.remove
-		  end
-		end
+        css('b').each do |node|
+          if node['class'] == "show"
+            node.remove
+          end
+        end
 				  	
-		# Put code blocks in <pre> tags
+        # Put code blocks in <pre> tags
         css('.code').each do |node|
           node.name = 'pre'
-	    end
+        end
 	    
-	    #remove "Hide inherited methods / properties
-	    css('a').each do |node|
-	      if node['class'] == 'toggle'
-	        node.remove
-	      end
-	    end
+        #remove "Hide inherited methods / properties
+        css('a').each do |node|
+          if node['class'] == 'toggle'
+            node.remove
+          end
+        end
 	    
         doc
       end
