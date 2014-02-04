11c3cae7f7898051e6cd45dab8e01065115ed7f5
Kirk Hansen <kirk.hansen@colorfx.net>
2014-02-04 10:26:21 -0600

    cleaning up indentation

--- lib/docs/filters/yii/entries.rb
+++ lib/docs/filters/yii/entries.rb
@@ -1,30 +1,30 @@
 module Docs
   class Yii
     class EntriesFilter < Docs::EntriesFilter
-     
+
       def get_name
         #class names exist in the <h1> content.
         name = at_css('h1').content.strip 
       end
-      
+
       def get_type
         #need to get the table with a class of summaryTable. Then we need the content of the first td in the first tr.
-	    type = css('table.summaryTable td')[0].content
-		type
+        type = css('table.summaryTable td')[0].content
+        type
       end
-      
+
       def additional_entries
-      	css('table.summaryTable tr[id]').inject [] do |entries,node|
-      	#need to ignore inherited methods and properties
-      	  if (node['class'] != 'inherited' and node.parent().parent()['class'] == "summary docMethod")
-      	    #name should be Class.method() id will take you to the link in the summary block.
-      	    name = slug + "." + node['id'] + "()"
-      	    entries << [name, node['id']]
-		  end			
-       	  entries
-        end   
-      end  
-         
+          css('table.summaryTable tr[id]').inject [] do |entries,node|
+          #need to ignore inherited methods and properties
+            if (node['class'] != 'inherited' and node.parent().parent()['class'] == "summary docMethod")
+              #name should be Class.method() id will take you to the link in the summary block.
+              name = slug + "." + node['id'] + "()"
+              entries << [name, node['id']]
+          end
+             entries
+        end
+      end
+
     end
   end
 end
