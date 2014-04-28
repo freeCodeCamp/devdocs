ed79e56e2e994e6f3ecac7e66759f7b564cc4e24
Thibaut <thibaut@me.com>
2014-04-27 22:03:49 -0400

    Update Ember.js documentation (1.5.1)

--- lib/docs/scrapers/ember.rb
+++ lib/docs/scrapers/ember.rb
@@ -3,7 +3,7 @@ module Docs
     self.name = 'Ember.js'
     self.slug = 'ember'
     self.type = 'ember'
-    self.version = '1.5.0'
+    self.version = '1.5.1'
     self.base_url = 'http://emberjs.com/api/'
 
     html_filters.push 'ember/clean_html', 'ember/entries', 'title'
