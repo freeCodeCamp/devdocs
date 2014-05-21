98aba4a05587c569addc0b3701aa6eaf3ed552a0
Romeo Van Snick <romeovs@gmail.com>
2014-05-21 10:58:28 +0200

    start adding haskell scraper

---
+++ lib/docs/scrapers/haskell.rb
@@ -0,0 +1,9 @@
+module Docs
+  class Haskell < UrlScraper
+    self.name = 'Haskell'
+    self.slug = 'haskell'
+    self.version = '7.8.2'
+    self.base_url = 'http://www.haskell.org/ghc/docs/7.8.2/html/libraries'
+
+    end
+end
