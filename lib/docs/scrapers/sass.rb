790a70e96110e72d0bde15cfd07599030cfff938
Thibaut <thibaut@me.com>
2014-07-01 09:37:54 -0400

    Update Sass documentation (3.3.9)

--- lib/docs/scrapers/sass.rb
+++ lib/docs/scrapers/sass.rb
@@ -1,7 +1,7 @@
 module Docs
   class Sass < UrlScraper
     self.type = 'yard'
-    self.version = '3.3.8'
+    self.version = '3.3.9'
     self.base_url = 'http://sass-lang.com/documentation/'
     self.root_path = 'file.SASS_REFERENCE.html'
 
