b4dbeae9fc8d2d811f9785729972b40c07a7fe32
Thibaut <thibaut@me.com>
2014-08-30 12:25:35 -0400

    Update Express.js documentation (4.8.7)

--- lib/docs/scrapers/express.rb
+++ lib/docs/scrapers/express.rb
@@ -2,7 +2,7 @@ module Docs
   class Express < UrlScraper
     self.name = 'Express'
     self.type = 'express'
-    self.version = '4.8.1'
+    self.version = '4.8.7'
     self.base_url = 'http://expressjs.com/4x/api.html'
 
     html_filters.push 'express/clean_html', 'express/entries', 'title'
