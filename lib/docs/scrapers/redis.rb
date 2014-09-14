28f3e4ea8cb86fa2975ca5dc66ef20c606b38afc
Thibaut <thibaut@me.com>
2014-09-14 14:46:22 -0400

    Update Redis documentation (2.8.15)

--- lib/docs/scrapers/redis.rb
+++ lib/docs/scrapers/redis.rb
@@ -1,7 +1,7 @@
 module Docs
   class Redis < UrlScraper
     self.type = 'redis'
-    self.version = 'up to 2.8.12'
+    self.version = 'up to 2.8.15'
     self.base_url = 'http://redis.io/commands'
 
     html_filters.push 'redis/entries', 'redis/clean_html', 'title'
