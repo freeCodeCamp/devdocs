0f3100389643b742d7456eb6ee861374f64a43b0
Thibaut <thibaut@me.com>
2014-10-12 09:31:24 -0400

    Update Less documentation (1.7.5)

--- lib/docs/scrapers/less.rb
+++ lib/docs/scrapers/less.rb
@@ -1,7 +1,7 @@
 module Docs
   class Less < UrlScraper
     self.type = 'less'
-    self.version = '1.7.4'
+    self.version = '1.7.5'
     self.base_url = 'http://lesscss.org'
     self.root_path = '/features'
     self.initial_paths = %w(/functions)
