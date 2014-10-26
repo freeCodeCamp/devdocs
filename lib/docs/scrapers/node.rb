a90077cdf12349f6238d2c36c1671c9d52c615a0
Thibaut <thibaut@me.com>
2014-10-26 18:04:09 -0400

    Update Node.js documentation (0.10.33)

--- lib/docs/scrapers/node.rb
+++ lib/docs/scrapers/node.rb
@@ -3,7 +3,7 @@ module Docs
     self.name = 'Node.js'
     self.slug = 'node'
     self.type = 'node'
-    self.version = '0.10.32'
+    self.version = '0.10.33'
     self.base_url = 'http://nodejs.org/api/'
 
     html_filters.push 'node/clean_html', 'node/entries', 'title'
