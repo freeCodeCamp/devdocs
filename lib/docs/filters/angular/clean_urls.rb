cb5580d33ccd8520d48a22dc09840c795efa74ce
Thibaut <thibaut@me.com>
2014-11-09 22:24:16 -0500

    Update Angular.js documentation (1.3.2)

--- lib/docs/filters/angular/clean_urls.rb
+++ lib/docs/filters/angular/clean_urls.rb
@@ -2,8 +2,8 @@ module Docs
   class Angular
     class CleanUrlsFilter < Filter
       def call
-        html.gsub! 'angularjs.org/1.3.1/docs/partials/api/', 'angularjs.org/1.3.1/docs/api/'
-        html.gsub! %r{angularjs.org/1.3.1/docs/api/(.+?)\.html}, 'angularjs.org/1.3.1/docs/api/\1'
+        html.gsub! 'angularjs.org/1.3.2/docs/partials/api/', 'angularjs.org/1.3.2/docs/api/'
+        html.gsub! %r{angularjs.org/1.3.2/docs/api/(.+?)\.html}, 'angularjs.org/1.3.2/docs/api/\1'
         html
       end
     end
