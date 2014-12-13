921162867612f3d62815c0d45665860392240449
Thibaut <thibaut@me.com>
2014-12-13 15:20:45 -0500

    Update Angular.js documentation (1.3.6)

--- lib/docs/scrapers/angular.rb
+++ lib/docs/scrapers/angular.rb
@@ -3,7 +3,7 @@ module Docs
     self.name = 'Angular.js'
     self.slug = 'angular'
     self.type = 'angular'
-    self.version = '1.3.4'
+    self.version = '1.3.6'
     self.base_url = "https://code.angularjs.org/#{version}/docs/partials/api/"
 
     html_filters.push 'angular/clean_html', 'angular/entries', 'title'
