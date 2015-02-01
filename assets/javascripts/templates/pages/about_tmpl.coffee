d8ada26d0818eba0794774f7c53315162aee50e9
Thibaut <thibaut@me.com>
2015-02-01 16:24:53 -0500

    Update Redis documentation (2.8.19)

--- assets/javascripts/templates/pages/about_tmpl.coffee
+++ assets/javascripts/templates/pages/about_tmpl.coffee
@@ -267,7 +267,7 @@ credits = [
     'https://raw.githubusercontent.com/facebook/react/master/LICENSE-docs'
   ], [
     'Redis',
-    '2009-2014 Salvatore Sanfilippo',
+    '2009-2015 Salvatore Sanfilippo',
     'CC BY-SA',
     'http://creativecommons.org/licenses/by-sa/4.0/'
   ], [
--- lib/docs/scrapers/redis.rb
+++ lib/docs/scrapers/redis.rb
@@ -1,7 +1,7 @@
 module Docs
   class Redis < UrlScraper
     self.type = 'redis'
-    self.version = 'up to 2.8.18'
+    self.version = 'up to 2.8.19'
     self.base_url = 'http://redis.io/commands'
 
     html_filters.push 'redis/entries', 'redis/clean_html', 'title'
@@ -12,7 +12,7 @@ module Docs
     options[:follow_links] = ->(filter) { filter.root_page? }
 
     options[:attribution] = <<-HTML
-      &copy; 2009&ndash;2014 Salvatore Sanfilippo<br>
+      &copy; 2009&ndash;2015 Salvatore Sanfilippo<br>
       Licensed under the Creative Commons Attribution-ShareAlike License 4.0.
     HTML
   end
