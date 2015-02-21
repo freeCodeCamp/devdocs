cf9e549fd8ef206eed9f5571ff812e3797a2c18e
Thibaut <thibaut@me.com>
2015-02-21 11:16:23 -0500

    Update Socket.io documentation (1.3.4)

--- lib/docs/scrapers/socketio.rb
+++ lib/docs/scrapers/socketio.rb
@@ -3,7 +3,7 @@ module Docs
     self.name = 'Socket.IO'
     self.slug = 'socketio'
     self.type = 'socketio'
-    self.version = '1.3.2'
+    self.version = '1.3.4'
     self.base_url = 'http://socket.io/docs/'
 
     html_filters.push 'socketio/clean_html', 'socketio/entries'
