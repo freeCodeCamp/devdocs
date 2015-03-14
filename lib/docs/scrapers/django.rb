ced2b4f7df99983f8764925f3c56d7bce21862f2
Thibaut <thibaut@me.com>
2015-03-14 18:09:48 -0400

    Update Django documentation (1.7.6)

--- lib/docs/scrapers/django.rb
+++ lib/docs/scrapers/django.rb
@@ -2,10 +2,14 @@ module Docs
   class Django < FileScraper
     self.name = 'Django'
     self.type = 'sphinx'
-    self.version = '1.7.4'
+    self.version = '1.7.6'
     self.dir = '/Users/Thibaut/DevDocs/Docs/Django'
     self.base_url = 'https://docs.djangoproject.com/en/1.7/'
     self.root_path = 'index.html'
+    self.links = {
+      home: 'https://www.djangoproject.com/',
+      code: 'https://github.com/django/django'
+    }
 
     html_filters.push 'django/entries', 'django/clean_html'
     text_filters.push 'django/fix_urls'
