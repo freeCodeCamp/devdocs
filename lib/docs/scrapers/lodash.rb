fe9ddf3bdd0476ad2ec3ec4b0387d6d6e6c12285
Thibaut <thibaut@me.com>
2015-03-29 10:43:38 -0400

    Update lodash documentation (3.6.0)

--- lib/docs/scrapers/lodash.rb
+++ lib/docs/scrapers/lodash.rb
@@ -3,7 +3,7 @@ module Docs
     self.name = 'lodash'
     self.slug = 'lodash'
     self.type = 'lodash'
-    self.version = '3.5.0'
+    self.version = '3.6.0'
     self.base_url = 'https://lodash.com/docs'
     self.links = {
       home: 'https://lodash.com/',
