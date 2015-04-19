08dcfe7047149b8fc86fef294029ce144bf21700
Thibaut <thibaut@me.com>
2015-04-19 10:47:12 -0400

    Update PHPUnit documentation (4.6)

--- lib/docs/scrapers/phpunit.rb
+++ lib/docs/scrapers/phpunit.rb
@@ -2,7 +2,7 @@ module Docs
   class Phpunit < UrlScraper
     self.name = 'PHPUnit'
     self.type = 'phpunit'
-    self.version = '4.5'
+    self.version = '4.6'
     self.base_url = "https://phpunit.de/manual/#{version}/en/"
     self.root_path = 'index.html'
 
