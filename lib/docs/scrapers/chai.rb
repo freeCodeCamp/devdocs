4b11604299c5fcae9043b3aa85917498d41b539f
Thibaut <thibaut@me.com>
2015-05-03 22:32:51 -0400

    Update Chai documentation (2.3.0)

--- lib/docs/scrapers/chai.rb
+++ lib/docs/scrapers/chai.rb
@@ -2,10 +2,14 @@ module Docs
   class Chai < UrlScraper
     self.name = 'Chai'
     self.type = 'chai'
-    self.version = '2.2.0'
+    self.version = '2.3.0'
     self.base_url = 'http://chaijs.com'
     self.root_path = '/api/'
     self.initial_paths = %w(/guide/installation/)
+    self.links = {
+      home: 'http://chaijs.com/',
+      code: 'https://github.com/chaijs/chai'
+    }
 
     html_filters.push 'chai/entries', 'chai/clean_html'
 
