c1725543aa591cf25721bf01aada332eab20ed13
Thibaut <thibaut@me.com>
2015-05-18 09:37:44 -0400

    Update Ember.js documentation (1.12.0)

--- lib/docs/scrapers/ember.rb
+++ lib/docs/scrapers/ember.rb
@@ -3,7 +3,7 @@ module Docs
     self.name = 'Ember.js'
     self.slug = 'ember'
     self.type = 'ember'
-    self.version = '1.11.0'
+    self.version = '1.12.0'
     self.base_url = 'http://emberjs.com/api/'
     self.links = {
       home: 'http://emberjs.com/',
