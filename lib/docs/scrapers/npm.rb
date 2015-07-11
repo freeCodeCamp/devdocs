6fd2904b5ff6ff10c3fd7126d36eb93dcc90cad5
Thibaut <thibaut@me.com>
2015-07-11 10:33:05 -0400

    Update npm documentation (2.13.0)

--- lib/docs/scrapers/npm.rb
+++ lib/docs/scrapers/npm.rb
@@ -2,7 +2,7 @@ module Docs
   class Npm < UrlScraper
     self.name = 'npm'
     self.type = 'npm'
-    self.version = '2.12.1'
+    self.version = '2.13.0'
     self.base_url = 'https://docs.npmjs.com/'
     self.links = {
       home: 'https://www.npmjs.com/',
