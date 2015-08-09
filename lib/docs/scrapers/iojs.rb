61775338cd350ed861f254d4e7be191c7586d169
Thibaut <thibaut@me.com>
2015-08-09 11:43:46 -0400

    Update io.js documentation (3.0.0)

--- lib/docs/scrapers/iojs.rb
+++ lib/docs/scrapers/iojs.rb
@@ -3,7 +3,7 @@ module Docs
     self.name = 'io.js'
     self.slug = 'iojs'
     self.type = 'node'
-    self.version = '2.5.0'
+    self.version = '3.0.0'
     self.base_url = 'https://iojs.org/api/'
     self.links = {
       home: 'https://iojs.org/en/index.html',
