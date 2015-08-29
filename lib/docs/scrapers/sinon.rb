c2cc4accace7d2b6aef5d73b610ed974a300a63b
Thibaut <thibaut@me.com>
2015-08-29 12:23:42 -0400

    Update Sinon documentation (1.16.1)

--- lib/docs/scrapers/sinon.rb
+++ lib/docs/scrapers/sinon.rb
@@ -2,7 +2,7 @@ module Docs
   class Sinon < UrlScraper
     self.name = 'Sinon'
     self.type = 'sinon'
-    self.version = '1.15.0'
+    self.version = '1.16.1'
     self.base_url = 'http://sinonjs.org/docs/'
     self.links = {
       home: 'http://sinonjs.org/',
