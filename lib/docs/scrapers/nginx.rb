74f5f23f8013f81021eec2120e6a190cabce0ecf
Thibaut <thibaut@me.com>
2015-10-12 10:49:47 -0400

    Update nginx documentation (1.9.5)

--- lib/docs/scrapers/nginx.rb
+++ lib/docs/scrapers/nginx.rb
@@ -2,7 +2,7 @@ module Docs
   class Nginx < UrlScraper
     self.name = 'nginx'
     self.type = 'nginx'
-    self.version = '1.9.4'
+    self.version = '1.9.5'
     self.base_url = 'http://nginx.org/en/docs/'
     self.links = {
       home: 'http://nginx.org/',
