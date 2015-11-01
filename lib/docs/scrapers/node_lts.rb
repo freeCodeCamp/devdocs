a91a2dee0591b372be0535e918071f3e57f44d26
Thibaut <thibaut@me.com>
2015-11-01 11:00:17 -0500

    Add Node.js LTS documentation

---
+++ lib/docs/scrapers/node_lts.rb
@@ -0,0 +1,9 @@
+module Docs
+  class NodeLts < Node
+    self.name = 'Node.js (LTS)'
+    self.slug = 'node_lts'
+    self.type = 'node'
+    self.version = '4.2.1'
+    self.base_url = 'https://nodejs.org/dist/v4.2.1/docs/api/'
+  end
+end
