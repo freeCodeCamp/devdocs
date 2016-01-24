f7b213c4d3fa00310d66e04d69198d461c38f8a7
Thibaut Courouble <thibaut@me.com>
2016-01-24 16:11:19 -0500

    Version Clojure documentation (1.7 and 1.8)
    Ref #25.

--- lib/docs/scrapers/clojure.rb
+++ lib/docs/scrapers/clojure.rb
@@ -1,8 +1,6 @@
 module Docs
   class Clojure < UrlScraper
     self.type = 'clojure'
-    self.release = '1.7'
-    self.base_url = 'http://clojure.github.io/clojure/'
     self.root_path = 'api-index.html'
 
     html_filters.push 'clojure/entries', 'clojure/clean_html'
@@ -14,5 +12,15 @@ module Docs
       &copy; Rich Hickey<br>
       Licensed under the Eclipse Public License 1.0.
     HTML
+
+    version '1.8' do
+      self.release = '1.8'
+      self.base_url = 'https://clojure.github.io/clojure/'
+    end
+
+    version '1.7' do
+      self.release = '1.7'
+      self.base_url = 'https://clojure.github.io/clojure/branch-clojure-1.7.0/'
+    end
   end
 end
