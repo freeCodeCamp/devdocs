63823b63424dc333c676145863716fe6dc0f1667
Thibaut Courouble <thibaut@me.com>
2016-02-06 17:15:56 -0500

    Update Angular.js documentation (1.5.0)

--- lib/docs/scrapers/angular.rb
+++ lib/docs/scrapers/angular.rb
@@ -5,7 +5,7 @@ module Docs
     self.name = 'Angular.js'
     self.slug = 'angular'
     self.type = 'angular'
-    self.release = '1.4.8'
+    self.release = '1.5.0'
     self.base_url = "https://code.angularjs.org/#{release}/docs/partials/api/"
 
     html_filters.push 'angular/clean_html', 'angular/entries', 'title'
