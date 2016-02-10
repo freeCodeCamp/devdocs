be232b666c23652b39cffda417fd0a5a496f0076
Yosi Attias <yosy101@gmail.com>
2016-02-10 21:27:03 +0200

    Bumping influxdb version to 0.10

--- lib/docs/scrapers/influxdb.rb
+++ lib/docs/scrapers/influxdb.rb
@@ -2,8 +2,8 @@ module Docs
   class Influxdb < UrlScraper
     self.name = 'InfluxDB'
     self.type = 'influxdb'
-    self.release = '0.9'
-    self.base_url = 'https://docs.influxdata.com/influxdb/v0.9/'
+    self.release = '0.10'
+    self.base_url = 'https://docs.influxdata.com/influxdb/v0.10/'
     
     html_filters.push 'influxdb/entries', 'influxdb/clean_html'
 
