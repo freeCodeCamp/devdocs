4c5ce5ffdd3fa71d48039f365346067fd1442cc9
Thibaut Courouble <thibaut@me.com>
2016-02-28 16:02:43 -0500

    Update npm documentation (3.7.5)

--- lib/docs/scrapers/npm.rb
+++ lib/docs/scrapers/npm.rb
@@ -2,7 +2,7 @@ module Docs
   class Npm < UrlScraper
     self.name = 'npm'
     self.type = 'npm'
-    self.release = '3.5.3'
+    self.release = '3.7.5'
     self.base_url = 'https://docs.npmjs.com/'
     self.links = {
       home: 'https://www.npmjs.com/',
