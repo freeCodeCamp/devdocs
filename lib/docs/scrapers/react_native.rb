70688f9b01aa2f6db2c328307d61743cb84f827d
Thibaut Courouble <thibaut@me.com>
2016-02-28 16:27:45 -0500

    Update React Native documentation (0.20)

--- lib/docs/scrapers/react_native.rb
+++ lib/docs/scrapers/react_native.rb
@@ -3,7 +3,7 @@ module Docs
     self.name = 'React Native'
     self.slug = 'react_native'
     self.type = 'react'
-    self.release = '0.19'
+    self.release = '0.20'
     self.base_url = 'https://facebook.github.io/react-native/docs/'
     self.root_path = 'getting-started.html'
     self.links = {
@@ -17,6 +17,11 @@ module Docs
       videos.html
       transforms.html
       troubleshooting.html)
+
+    options[:fix_urls] = ->(url) {
+      url.sub! 'docs/docs', 'docs'
+      url
+    }
 
     options[:attribution] = <<-HTML
       &copy; 2016 Facebook Inc.<br>
