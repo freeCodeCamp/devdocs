3317df29c1c10e7f6d7f52b1e78cda9e5fdae9ee
Thibaut Courouble <thibaut@me.com>
2016-04-03 09:53:37 -0400

    Update Ramda documentation (0.20.1)

--- lib/docs/filters/ramda/entries.rb
+++ lib/docs/filters/ramda/entries.rb
@@ -3,7 +3,7 @@ module Docs
     class EntriesFilter < Docs::EntriesFilter
       def additional_entries
         css('ul.toc li').map do |node|
-          ["R.#{node['data-name']}", node['data-name'], node['data-category']]
+          ["R.#{node['data-name']}", node['data-name'], node['data-category'].sub('Relaction', 'Relation')]
         end
       end
     end
