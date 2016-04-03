471211e47761d249332d19031372769a3667992b
Thibaut Courouble <thibaut@me.com>
2016-04-03 11:38:18 -0400

    Update PHP documentation (7.0.5)

--- lib/docs/scrapers/php.rb
+++ lib/docs/scrapers/php.rb
@@ -4,7 +4,7 @@ module Docs
 
     self.name = 'PHP'
     self.type = 'php'
-    self.release = 'up to 7.0.3'
+    self.release = 'up to 7.0.5'
     self.base_url = 'https://secure.php.net/manual/en/'
     self.root_path = 'index.html'
     self.initial_paths = %w(
