1cd25364f7699d3b5d7442ba786695b643650647
Matthias Guenther <matthias@wikimatze.de>
2016-06-04 22:59:56 +0200

    Remove 'collapse' and 'show all' links

--- lib/docs/filters/padrino/clean_html.rb
+++ lib/docs/filters/padrino/clean_html.rb
@@ -2,6 +2,8 @@ module Docs
   class Padrino
     class CleanHtmlFilter < Filter
       def call
+        css('.summary_toggle').remove
+        css('.inheritanceTree').remove
         at_css('#content')
       end
     end
