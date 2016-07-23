a69f091a3bc4aaee0fc4946ca0cc69b690fd3cb3
Thibaut Courouble <thibaut@me.com>
2016-07-23 18:21:17 -0400

    Update Go documentation (1.6.3)

--- lib/docs/scrapers/go.rb
+++ lib/docs/scrapers/go.rb
@@ -1,7 +1,7 @@
 module Docs
   class Go < UrlScraper
     self.type = 'go'
-    self.release = '1.6.0'
+    self.release = '1.6.3'
     self.base_url = 'https://golang.org/pkg/'
     self.links = {
       home: 'https://golang.org/',
