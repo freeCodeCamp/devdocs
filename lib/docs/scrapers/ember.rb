f5687aa17dd16e006a389de12e12e9433de27d6c
Thibaut Courouble <thibaut@me.com>
2016-07-31 15:24:46 -0400

    Update Ember.js documentation (2.7.0)

--- lib/docs/scrapers/ember.rb
+++ lib/docs/scrapers/ember.rb
@@ -3,7 +3,7 @@ module Docs
     self.name = 'Ember.js'
     self.slug = 'ember'
     self.type = 'ember'
-    self.release = '2.6.1'
+    self.release = '2.7.0'
     self.base_url = 'http://emberjs.com/api/'
     self.links = {
       home: 'http://emberjs.com/',
