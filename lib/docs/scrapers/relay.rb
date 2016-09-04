b41a179666bbd0c08ab4971c08a38e9720cdf690
Thibaut Courouble <thibaut@me.com>
2016-09-04 10:52:27 -0400

    Update Relay documentation (0.9.3)

--- lib/docs/scrapers/relay.rb
+++ lib/docs/scrapers/relay.rb
@@ -1,7 +1,7 @@
 module Docs
   class Relay < React
     self.type = 'react'
-    self.release = '0.9.2'
+    self.release = '0.9.3'
     self.base_url = 'https://facebook.github.io/relay/docs/'
     self.root_path = 'getting-started.html'
     self.links = {
