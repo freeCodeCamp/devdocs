4a93e291a0aae2e328838b7a3ac55291b776a995
Thibaut Courouble <thibaut@me.com>
2016-09-17 10:55:40 -0400

    Update React Native documentation (0.33)

--- lib/docs/scrapers/react_native.rb
+++ lib/docs/scrapers/react_native.rb
@@ -3,7 +3,7 @@ module Docs
     self.name = 'React Native'
     self.slug = 'react_native'
     self.type = 'react'
-    self.release = '0.32'
+    self.release = '0.33'
     self.base_url = 'https://facebook.github.io/react-native/docs/'
     self.root_path = 'getting-started.html'
     self.links = {
