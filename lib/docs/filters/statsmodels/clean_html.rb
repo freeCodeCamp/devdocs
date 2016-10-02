06f5faeb185dcbebccb51196ef3ee1fec32aad73
japborst <japborst@gmail.com>
2016-10-02 18:31:55 +0200

    Statsmodels support

---
+++ lib/docs/filters/statsmodels/clean_html.rb
@@ -0,0 +1,18 @@
+module Docs
+  class Statsmodels
+    class CleanHtmlFilter < Filter
+      def call
+        @doc = at_css('.body')
+
+        if root_page?
+            at_css('h1').content = 'Statsmodels'
+            at_css('#basic-documentation').remove
+            at_css('#table-of-contents').remove
+            at_css('#indices-and-tables').remove
+        end
+
+        doc
+      end
+    end
+  end
+end
---
+++ lib/docs/scrapers/statsmodels.rb
@@ -0,0 +1,26 @@
+module Docs
+  class Statsmodels < UrlScraper
+    self.name = 'Statsmodels'
+    self.type = 'sphinx'
+    self.release = '0.6.1'
+    self.base_url = "http://statsmodels.sourceforge.net/stable/"
+    self.root_path = 'index.html'
+    self.links = {
+      home: 'http://statsmodels.sourceforge.net/',
+      code: 'https://github.com/statsmodels/statsmodels/'
+    }
+
+    html_filters.push 'statsmodels/entries', 'statsmodels/clean_html', 'sphinx/clean_html'
+
+    options[:skip] = %w(about.html)
+    options[:skip_patterns] = [/\Arelease/, /\Adev/, /\A_modules/, /\Adatasets/]
+
+    options[:attribution] = <<-HTML
+      &copy; 2009&ndash;2012 Statsmodels Developers<br>
+      &copy; 2006&ndash;2008 Scipy Developers<br>
+      &copy; 2006 Jonathan E. Taylor<br>
+      Licensed under the 3-clause BSD License.
+    HTML
+
+  end
+end
