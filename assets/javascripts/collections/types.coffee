50e138bd67b4002083d25942d9d5ebfc1475d05d
Thibaut Courouble <thibaut@me.com>
2016-10-02 11:13:41 -0400

    Update PostgreSQL documentation (9.6.0, 9.5.4, 9.4.9)

--- assets/javascripts/collections/types.coffee
+++ assets/javascripts/collections/types.coffee
@@ -8,9 +8,12 @@ class app.collections.Types extends app.Collection
     result.filter (e) -> e.length > 0
 
   GUIDES_RGX = /(^|\()(guides?|tutorials?|reference|book|getting\ started|manual)($|[\):])/i
+  APPENDIX_RGX = /appendix/i
 
   _groupFor: (type) ->
     if GUIDES_RGX.test(type.name)
       0
+    else if APPENDIX_RGX.test(type.name)
+      2
     else
       1
--- lib/docs/scrapers/postgresql.rb
+++ lib/docs/scrapers/postgresql.rb
@@ -5,7 +5,7 @@ module Docs
     self.name = 'PostgreSQL'
     self.type = 'postgres'
     self.root_path = 'reference.html'
-    self.initial_paths = %w(sql.html admin.html internals.html)
+    self.initial_paths = %w(sql.html admin.html internals.html appendixes.html tutorial.html)
 
     html_filters.insert_before 'normalize_urls', 'postgresql/extract_metadata'
     html_filters.push 'postgresql/clean_html', 'postgresql/entries', 'title'
@@ -23,10 +23,13 @@ module Docs
       supported-platforms.html
       error-message-reporting.html
       error-style-guide.html
-      plhandler.html)
+      plhandler.html
+      sourcerepo.html
+      git.html
+      bug-reporting.html
+      client-interfaces.html)
 
     options[:skip_patterns] = [
-      /\Atutorial/,
       /\Ainstall/,
       /\Aregress/,
       /\Aprotocol/,
@@ -35,21 +38,32 @@ module Docs
       /\Afdw/,
       /\Atablesample/,
       /\Acustom-scan/,
-      /\Abki/ ]
+      /\Abki/,
+      /\Arelease/,
+      /\Acontrib-prog/,
+      /\Aexternal/,
+      /\Adocguide/,
+      /\Afeatures/,
+      /\Aunsupported-features/ ]
 
     options[:attribution] = <<-HTML
       &copy; 1996&ndash;2016 The PostgreSQL Global Development Group<br>
       Licensed under the PostgreSQL License.
     HTML
 
+    version '9.6' do
+      self.release = '9.6.0'
+      self.base_url = 'https://www.postgresql.org/docs/9.6/static/'
+    end
+
     version '9.5' do
-      self.release = '9.5'
-      self.base_url = 'http://www.postgresql.org/docs/9.5/static/'
+      self.release = '9.5.4'
+      self.base_url = 'https://www.postgresql.org/docs/9.5/static/'
     end
 
     version '9.4' do
-      self.release = '9.4'
-      self.base_url = 'http://www.postgresql.org/docs/9.4/static/'
+      self.release = '9.4.9'
+      self.base_url = 'https://www.postgresql.org/docs/9.4/static/'
     end
   end
 end
