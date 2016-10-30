2259123f5602f16f0adb1308e245f5859157143d
Thibaut Courouble <thibaut@me.com>
2016-10-30 17:08:27 -0400

    Update Moment.js documentation (2.15.2)

--- lib/docs/scrapers/moment.rb
+++ lib/docs/scrapers/moment.rb
@@ -3,7 +3,7 @@ module Docs
     self.name = 'Moment.js'
     self.slug = 'moment'
     self.type = 'moment'
-    self.release = '2.15.0'
+    self.release = '2.15.2'
     self.base_url = 'http://momentjs.com/docs/'
 
     html_filters.push 'moment/clean_html', 'moment/entries', 'title'
