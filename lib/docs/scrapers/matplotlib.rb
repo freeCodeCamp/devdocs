718fa9996154db2462afbc271f4fa3f89503b187
japborst <japborst@gmail.com>
2016-12-27 11:47:50 +0100

    Matplotlib toolkits support

--- lib/docs/scrapers/matplotlib.rb
+++ lib/docs/scrapers/matplotlib.rb
@@ -1,5 +1,7 @@
 module Docs
   class Matplotlib < UrlScraper
+    include MultipleBaseUrls
+
     self.name = 'Matplotlib'
     self.type = 'sphinx'
     self.root_path = 'index.html'
@@ -11,7 +13,7 @@ module Docs
     html_filters.push 'matplotlib/entries', 'sphinx/clean_html'
 
     options[:container] = '.body'
-    options[:skip] = %w(api_changes.html)
+    options[:skip] = %w(api_changes.html tutorial.html faq.html)
 
     options[:attribution] = <<-HTML
       &copy; 2012&ndash;2016 Matplotlib Development Team. All rights reserved.<br>
@@ -20,7 +22,11 @@ module Docs
 
     version '1.5' do
       self.release = '1.5.1'
-      self.base_url = 'http://matplotlib.org/1.5.1/api/'
+      self.base_urls = [
+        'http://matplotlib.org/1.5.1/api/',
+        'http://matplotlib.org/1.5.1/mpl_toolkits/mplot3d/',
+        'http://matplotlib.org/1.5.1/mpl_toolkits/axes_grid/api/'
+      ]
     end
   end
 end
