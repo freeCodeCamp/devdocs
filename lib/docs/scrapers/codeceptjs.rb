13a170bfcf932356bb02a0a83b283522f00f5cfb
Thibaut Courouble <thibaut@me.com>
2017-02-05 16:30:47 -0500

    Update CodeceptJS documentation (0.4.16)

--- lib/docs/scrapers/codeceptjs.rb
+++ lib/docs/scrapers/codeceptjs.rb
@@ -3,7 +3,7 @@ module Docs
     self.name = 'CodeceptJS'
     self.type = 'codeceptjs'
     self.root_path = 'index.html'
-    self.release = '0.4.13'
+    self.release = '0.4.16'
     self.base_url = 'http://codecept.io/'
     self.links = {
       home: 'http://codecept.io/',
