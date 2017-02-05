53b370581bacb7b3820aec03c6d1193691e32d54
Thibaut Courouble <thibaut@me.com>
2017-02-05 16:02:12 -0500

    Update Node.js documentation (7.5.0, 6.9.5, 4.7.3)

--- lib/docs/scrapers/node.rb
+++ lib/docs/scrapers/node.rb
@@ -23,17 +23,17 @@ module Docs
     HTML
 
     version do
-      self.release = '7.4.0'
+      self.release = '7.5.0'
       self.base_url = 'https://nodejs.org/dist/latest-v7.x/docs/api/'
     end
 
     version '6 LTS' do
-      self.release = '6.9.4'
+      self.release = '6.9.5'
       self.base_url = 'https://nodejs.org/dist/latest-v6.x/docs/api/'
     end
 
     version '4 LTS' do
-      self.release = '4.7.2'
+      self.release = '4.7.3'
       self.base_url = 'https://nodejs.org/dist/latest-v4.x/docs/api/'
     end
   end
