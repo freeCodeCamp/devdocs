d0e4f2a9a4309502cbaeedc08b3ea644c1ce5d9b
Thibaut Courouble <thibaut@me.com>
2017-03-12 15:24:02 -0400

    Update Knockout.js documentation (3.4.2)

--- lib/docs/scrapers/knockout.rb
+++ lib/docs/scrapers/knockout.rb
@@ -3,7 +3,7 @@ module Docs
     self.name = 'Knockout.js'
     self.slug = 'knockout'
     self.type = 'knockout'
-    self.release = '3.4.1'
+    self.release = '3.4.2'
     self.base_url = 'http://knockoutjs.com/documentation/'
     self.root_path = 'introduction.html'
 
