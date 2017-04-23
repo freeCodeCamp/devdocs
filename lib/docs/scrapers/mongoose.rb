7fbb761f0a036a2ebc34a2f4d8a9c78f006aa2c9
Thibaut Courouble <thibaut@me.com>
2017-04-23 10:27:57 -0400

    Update Mongoose documentation (4.9.5)

--- lib/docs/scrapers/mongoose.rb
+++ lib/docs/scrapers/mongoose.rb
@@ -2,7 +2,7 @@ module Docs
   class Mongoose < UrlScraper
     self.name = 'Mongoose'
     self.type = 'mongoose'
-    self.release = '4.9.1'
+    self.release = '4.9.5'
     self.base_url = 'http://mongoosejs.com/docs/'
     self.root_path = 'index.html'
     self.initial_paths = %w(guide.html api.html)
