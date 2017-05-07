ad2f9a4553d1809e0fe38bb119dec057df66b1e3
Thibaut Courouble <thibaut@me.com>
2017-05-07 11:41:50 -0400

    Update React Native documentation (0.44)

--- lib/docs/filters/react_native/clean_html.rb
+++ lib/docs/filters/react_native/clean_html.rb
@@ -6,10 +6,18 @@ module Docs
           css('h1 ~ *').remove
         end
 
-        css('center > .button', 'p:contains("short survey")', 'iframe', '.embedded-simulator').remove
+        css('center > .button', 'p:contains("short survey")', 'iframe', '.embedded-simulator', '.deprecatedIcon').remove
 
         css('h4.methodTitle').each do |node|
           node.name = 'h3'
+        end
+
+        css('div:not([class])', 'span:not([class])').each do |node|
+          node.before(node.children).remove
+        end
+
+        css('ul').each do |node|
+          node.before(node.children).remove if node.at_css('> p', '> h2')
         end
 
         doc
--- lib/docs/scrapers/react_native.rb
+++ lib/docs/scrapers/react_native.rb
@@ -3,7 +3,7 @@ module Docs
     self.name = 'React Native'
     self.slug = 'react_native'
     self.type = 'react'
-    self.release = '0.43'
+    self.release = '0.44'
     self.base_url = 'https://facebook.github.io/react-native/docs/'
     self.root_path = 'getting-started.html'
     self.links = {
