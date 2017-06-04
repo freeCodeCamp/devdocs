2ce7d9e5e25fbef88063578d9118b4822bdf60e7
Thibaut Courouble <thibaut@me.com>
2017-06-04 11:40:53 -0400

    Update npm documentation (5.0.2)

--- lib/docs/scrapers/npm.rb
+++ lib/docs/scrapers/npm.rb
@@ -2,7 +2,7 @@ module Docs
   class Npm < UrlScraper
     self.name = 'npm'
     self.type = 'npm'
-    self.release = '4.6.1'
+    self.release = '5.0.2'
     self.base_url = 'https://docs.npmjs.com/'
     self.force_gzip = true
     self.links = {
