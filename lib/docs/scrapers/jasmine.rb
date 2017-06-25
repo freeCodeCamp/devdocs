6a77e253f5b4de4ff2ac496e34d43a796350c2bb
Thibaut Courouble <thibaut@me.com>
2017-06-25 09:53:11 -0400

    Update Jasmine documentation (2.6.4)

--- lib/docs/scrapers/jasmine.rb
+++ lib/docs/scrapers/jasmine.rb
@@ -1,7 +1,7 @@
 module Docs
   class Jasmine < UrlScraper
     self.type = 'jasmine'
-    self.release = '2.6.1'
+    self.release = '2.6.4'
     self.base_url = 'https://jasmine.github.io/api/2.6/'
     self.root_path = 'index.html'
     self.links = {
