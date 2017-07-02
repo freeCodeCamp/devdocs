ee21c0bf299010a771a9884a466311b9f46451fd
Thibaut Courouble <thibaut@me.com>
2017-07-02 19:26:00 -0400

    Add frozen_string_literal to subscriber files"

--- lib/docs/subscribers/doc_subscriber.rb
+++ lib/docs/subscribers/doc_subscriber.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Docs
   class DocSubscriber < Subscriber
     self.namespace = 'doc'
--- lib/docs/subscribers/store_subscriber.rb
+++ lib/docs/subscribers/store_subscriber.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Docs
   class StoreSubscriber < Subscriber
     self.namespace = 'store'
