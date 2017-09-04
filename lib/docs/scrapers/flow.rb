c302e33d75954fb05aae79eb2049445772df04d2
Thibaut Courouble <thibaut@me.com>
2017-09-04 09:39:28 -0400

    Update Flow documentation (0.54.0)

--- lib/docs/scrapers/flow.rb
+++ lib/docs/scrapers/flow.rb
@@ -1,7 +1,7 @@
 module Docs
   class Flow < UrlScraper
     self.type = 'flow'
-    self.release = '0.53.1'
+    self.release = '0.54.0'
     self.base_url = 'https://flow.org/en/docs/'
     self.links = {
       home: 'https://flow.org/',
