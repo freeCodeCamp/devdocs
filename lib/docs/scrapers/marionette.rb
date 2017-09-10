18928da77f894e4123c9bd80521cc7b58e04dbf1
Thibaut Courouble <thibaut@me.com>
2017-09-10 11:00:11 -0400

    Update Marionette.js documentation (3.4.3)

--- lib/docs/scrapers/marionette.rb
+++ lib/docs/scrapers/marionette.rb
@@ -19,7 +19,7 @@ module Docs
     HTML
 
     version '3' do
-      self.release = '3.3.1'
+      self.release = '3.4.3'
       self.base_url = "https://marionettejs.com/docs/v#{release}/"
 
       html_filters.push 'marionette/entries_v3'
