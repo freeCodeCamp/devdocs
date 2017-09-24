a366e4a9992aa9a83516a520f584bb10b579e2f2
Thibaut Courouble <thibaut@me.com>
2017-09-24 13:49:02 -0400

    Update PostgreSQL documentation (9.6.5)

--- lib/docs/scrapers/postgresql.rb
+++ lib/docs/scrapers/postgresql.rb
@@ -56,17 +56,17 @@ module Docs
     HTML
 
     version '9.6' do
-      self.release = '9.6.3'
+      self.release = '9.6.5'
       self.base_url = 'https://www.postgresql.org/docs/9.6/static/'
     end
 
     version '9.5' do
-      self.release = '9.5.7'
+      self.release = '9.5.9'
       self.base_url = 'https://www.postgresql.org/docs/9.5/static/'
     end
 
     version '9.4' do
-      self.release = '9.4.12'
+      self.release = '9.4.14'
       self.base_url = 'https://www.postgresql.org/docs/9.4/static/'
     end
   end
