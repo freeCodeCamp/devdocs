6b4d4518979ac48f2a12007d5f4b133d0cff23db
Thibaut Courouble <thibaut@me.com>
2017-09-24 13:03:28 -0400

    Update Redis documentation (4.0.2)

--- lib/docs/scrapers/redis.rb
+++ lib/docs/scrapers/redis.rb
@@ -1,7 +1,7 @@
 module Docs
   class Redis < UrlScraper
     self.type = 'redis'
-    self.release = 'up to 4.0.1'
+    self.release = '4.0.2'
     self.base_url = 'https://redis.io/commands'
     self.links = {
       home: 'https://redis.io/',
