158563d2edae954755bfcf4d477a6029d06f22d5
Thibaut Courouble <thibaut@me.com>
2017-10-07 11:11:40 -0400

    Update Yarn documentation (1.1.0)

--- lib/docs/scrapers/yarn.rb
+++ lib/docs/scrapers/yarn.rb
@@ -1,7 +1,7 @@
 module Docs
   class Yarn < UrlScraper
     self.type = 'yarn'
-    self.release = '1.0.1'
+    self.release = '1.1.0'
     self.base_url = 'https://yarnpkg.com/en/docs/'
     self.links = {
       home: 'https://yarnpkg.com/',
