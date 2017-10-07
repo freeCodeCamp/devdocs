7c84f90dd5b71db25c83ae71b669ee22ddd17935
Thibaut Courouble <thibaut@me.com>
2017-10-07 12:10:44 -0400

    Update Elixir documentation (1.5.2)

--- lib/docs/scrapers/elixir.rb
+++ lib/docs/scrapers/elixir.rb
@@ -34,7 +34,7 @@ module Docs
     end
 
     version '1.5' do
-      self.release = '1.5.1'
+      self.release = '1.5.2'
       self.base_urls = [
         "https://hexdocs.pm/elixir/#{release}/",
         "https://hexdocs.pm/eex/#{release}/",
