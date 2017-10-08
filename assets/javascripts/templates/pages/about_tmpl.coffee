d91e2f98b4c3912b0e9af5900ecd4548de10fd6f
Thibaut Courouble <thibaut@me.com>
2017-10-08 11:37:24 -0400

    Update Nokogiri documentation (1.8.1)

--- assets/javascripts/templates/pages/about_tmpl.coffee
+++ assets/javascripts/templates/pages/about_tmpl.coffee
@@ -439,7 +439,7 @@ credits = [
     'https://raw.githubusercontent.com/nodejs/node/master/LICENSE'
   ], [
     'Nokogiri',
-    '2008-2016 Aaron Patterson, Mike Dalessio, Charles Nutter, Sergio Arbeo, Patrick Mahoney, Yoko Harada, Akinori Musha, John Shahid',
+    '2008-2017 Aaron Patterson, Mike Dalessio, Charles Nutter, Sergio Arbeo, Patrick Mahoney, Yoko Harada, Akinori Musha, John Shahid',
     'MIT',
     'https://raw.githubusercontent.com/sparklemotion/nokogiri/master/LICENSE.txt'
   ], [
--- lib/docs/scrapers/nokogiri2.rb
+++ lib/docs/scrapers/nokogiri2.rb
@@ -2,7 +2,7 @@ module Docs
   class Nokogiri2 < Rdoc
     self.name = 'Nokogiri'
     self.slug = 'nokogiri'
-    self.release = '1.7.0'
+    self.release = '1.8.1'
     self.dir = '/Users/Thibaut/DevDocs/Docs/RDoc/Nokogiri'
 
     html_filters.replace 'rdoc/entries', 'nokogiri2/entries'
@@ -11,7 +11,7 @@ module Docs
     options[:only_patterns] = [/\ANokogiri/, /\AXSD/]
 
     options[:attribution] = <<-HTML
-      &copy; 2008&ndash;2016 Aaron Patterson, Mike Dalessio, Charles Nutter, Sergio Arbeo<br>
+      &copy; 2008&ndash;2017 Aaron Patterson, Mike Dalessio, Charles Nutter, Sergio Arbeo<br>
       Patrick Mahoney, Yoko Harada, Akinori Musha, John Shahid<br>
       Licensed under the MIT License.
     HTML
