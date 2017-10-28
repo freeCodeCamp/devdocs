0f681120be5d00864c2609e9b4785f182ceebec0
Jed Fox <git@twopointzero.us>
2017-10-28 11:27:42 -0400

    Add the Jekyll docs

---
+++ lib/docs/filters/jekyll/entries.rb
@@ -0,0 +1,29 @@
+module Docs
+  class Jekyll
+    class EntriesFilter < Docs::EntriesFilter
+
+      def get_name
+        at_css('h1').content
+      end
+
+      def get_type
+        if /continuous-integration/.match(slug)
+          'Deployment'
+        else
+          nav_link = doc.document # document
+            .at_css('aside li.current') # item in navbar
+
+          if nav_link
+            nav_link
+              .parent # <ul> in navbar
+              .previous_element # header before <ul>
+              .content # category
+          else
+            'Miscellaneous'
+          end
+        end
+      end
+
+    end
+  end
+end
