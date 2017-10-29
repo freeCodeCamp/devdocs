a92e01c731483313a2cb2076e93974970a78555e
Thibaut Courouble <thibaut@me.com>
2017-10-29 14:11:09 -0400

    Update Jasmine documentation (2.8.0)

--- lib/docs/scrapers/jasmine.rb
+++ lib/docs/scrapers/jasmine.rb
@@ -2,7 +2,7 @@ module Docs
   class Jasmine < UrlScraper
     self.type = 'jasmine'
     self.release = '2.8.0'
-    self.base_url = 'https://jasmine.github.io/api/2.7/'
+    self.base_url = 'https://jasmine.github.io/api/2.8/'
     self.root_path = 'index.html'
     self.links = {
       home: 'https://jasmine.github.io/',
