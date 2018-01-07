01d0f1cda4b503b8b7727d6f9f54e38e5fff54a0
Thibaut Courouble <thibaut@me.com>
2018-01-07 18:32:26 -0500

    Update Git documentation (2.15.1)

--- lib/docs/scrapers/git.rb
+++ lib/docs/scrapers/git.rb
@@ -1,7 +1,7 @@
 module Docs
   class Git < UrlScraper
     self.type = 'git'
-    self.release = '2.14.3'
+    self.release = '2.15.1'
     self.base_url = 'https://git-scm.com/docs'
     self.initial_paths = %w(/git.html)
     self.links = {
