8cab0e08c9a6e4cf3fdf6be6c31369048c868ae4
Thibaut Courouble <thibaut@me.com>
2018-03-18 11:25:08 -0400

    Update webpack documentation (4.1.1)

--- lib/docs/scrapers/webpack.rb
+++ lib/docs/scrapers/webpack.rb
@@ -4,7 +4,7 @@ module Docs
     self.type = 'webpack'
 
     version do
-      self.release = '4.0.0'
+      self.release = '4.1.1'
       self.base_url = 'https://webpack.js.org/'
       self.root_path = 'guides/'
       self.initial_paths = %w(
