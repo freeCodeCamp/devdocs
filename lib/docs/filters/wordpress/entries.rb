c7da05580e82c6a7f7f0a7b958b2a49e4fc9d251
Szabolcs Légrádi <szabolcs.legradi@gmail.com>
2018-03-25 22:30:37 +0200

    Reduce number of types

--- lib/docs/filters/wordpress/entries.rb
+++ lib/docs/filters/wordpress/entries.rb
@@ -12,7 +12,13 @@ module Docs
       end
 
       def get_type
-        if breadcrumbs.size > 1
+        if subpath.starts_with?('classes')
+          'Classes'
+        elsif subpath.starts_with?('hooks')
+          'Hooks'
+        elsif subpath.starts_with?('functions')
+          'Functions'
+        elsif breadcrumbs.size > 1
           breadcrumbs.drop(1).join(': ')
         else
           at_css('.breadcrumbs .trail-end').content
