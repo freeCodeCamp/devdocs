7c70e63041a67426ec129cadb008550edd44872d
Thibaut Courouble <thibaut@me.com>
2018-05-13 20:26:11 -0400

    Update Bootstrap documentation (4.1.1)

--- lib/docs/scrapers/bootstrap.rb
+++ lib/docs/scrapers/bootstrap.rb
@@ -16,8 +16,8 @@ module Docs
     HTML
 
     version '4' do
-      self.release = '4.1.0'
-      self.base_url = 'https://getbootstrap.com/docs/4.0/'
+      self.release = '4.1.1'
+      self.base_url = 'https://getbootstrap.com/docs/4.1/'
       self.root_path = 'getting-started/introduction/'
 
       html_filters.push 'bootstrap/entries_v4', 'bootstrap/clean_html_v4'
