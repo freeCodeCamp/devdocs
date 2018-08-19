7b64b43ad346f237558ae21090559062260b54da
Thibaut Courouble <thibaut@me.com>
2018-08-19 18:07:37 -0400

    Update TypeScript documentation (3.0.1)

--- lib/docs/scrapers/typescript.rb
+++ lib/docs/scrapers/typescript.rb
@@ -2,7 +2,7 @@ module Docs
   class Typescript < UrlScraper
     self.name = 'TypeScript'
     self.type = 'typescript'
-    self.release = '2.9.0'
+    self.release = '3.0.1'
     self.base_url = 'https://www.typescriptlang.org/docs/'
     self.root_path = 'tutorial.html'
     self.links = {
