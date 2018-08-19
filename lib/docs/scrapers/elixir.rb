2c96165a93f6b984841540bfe35562478b4d0e85
Thibaut Courouble <thibaut@me.com>
2018-08-19 18:12:41 -0400

    Update Elixir documentation (1.7.2)

--- lib/docs/scrapers/elixir.rb
+++ lib/docs/scrapers/elixir.rb
@@ -33,6 +33,19 @@ module Docs
         "https://elixir-lang.org/getting-started/introduction.html" ]
     end
 
+    version '1.7' do
+      self.release = '1.7.2'
+      self.base_urls = [
+        "https://hexdocs.pm/elixir/#{release}/",
+        "https://hexdocs.pm/eex/#{release}/",
+        "https://hexdocs.pm/ex_unit/#{release}/",
+        "https://hexdocs.pm/iex/#{release}/",
+        "https://hexdocs.pm/logger/#{release}/",
+        "https://hexdocs.pm/mix/#{release}/",
+        'https://elixir-lang.org/getting-started/'
+      ]
+    end
+
     version '1.6' do
       self.release = '1.6.5'
       self.base_urls = [
