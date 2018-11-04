df8e46cfe546e8cf499e78989ff59d9af3717f27
Thibaut Courouble <thibaut@me.com>
2018-11-04 09:39:36 -0500

    Update ESLint documentation (5.8.0)

--- lib/docs/scrapers/eslint.rb
+++ lib/docs/scrapers/eslint.rb
@@ -2,7 +2,7 @@ module Docs
   class Eslint < UrlScraper
     self.name = 'ESLint'
     self.type = 'simple'
-    self.release = '4.19.0'
+    self.release = '5.8.0'
     self.base_url = 'https://eslint.org/docs/'
     self.root_path = 'user-guide/getting-started'
     self.links = {
