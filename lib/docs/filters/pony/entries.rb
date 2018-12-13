dea577a1a3e36b4052cb4c82ec0cbbd7e7398f80
Harm Aarts <harmaarts@gmail.com>
2018-12-13 22:24:34 +0100

    Fix type naming and remove debug statement

--- lib/docs/filters/pony/entries.rb
+++ lib/docs/filters/pony/entries.rb
@@ -2,12 +2,11 @@ module Docs
   class Pony
     class EntriesFilter < Docs::EntriesFilter
       def get_name
-        binding.pry
         context[:html_title].sub(/ - .*/, '')
       end
 
       def get_type
-        subpath.split('-')[0][1..-1]
+        subpath.split('-')[0][0..-1]
       end
     end
   end
