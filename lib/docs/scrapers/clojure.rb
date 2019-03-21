8c80bb8e31d2c591cca1c26dddb6d56b849c46be
Leandro Doctors <ldoctors@gmail.com>
2019-03-21 04:19:19 -0300

    Clojure: show version status (stable/legacy)

--- lib/docs/scrapers/clojure.rb
+++ lib/docs/scrapers/clojure.rb
@@ -14,22 +14,22 @@ module Docs
     HTML
 
     version '1.10' do
-      self.release = '1.10'
+      self.release = '1.10 (stable)'
       self.base_url = 'https://clojure.github.io/clojure/'
     end
 
     version '1.9' do
-      self.release = '1.9'
+      self.release = '1.9 (legacy)'
       self.base_url = 'https://clojure.github.io/clojure/branch-clojure-1.9.0/'
     end
 
     version '1.8' do
-      self.release = '1.8'
+      self.release = '1.8 (legacy)'
       self.base_url = 'https://clojure.github.io/clojure/branch-clojure-1.8.0/'
     end
 
     version '1.7' do
-      self.release = '1.7'
+      self.release = '1.7 (legacy)'
       self.base_url = 'https://clojure.github.io/clojure/branch-clojure-1.7.0/'
     end
   end
