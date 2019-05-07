ec4e867053ac428f0a9c97a0d508222119112403
Matthias Günther <matze@wikimatze.de>
2019-05-07 08:35:25 +0200

    Update padrino to 0.14.4

--- lib/docs/scrapers/padrino.rb
+++ lib/docs/scrapers/padrino.rb
@@ -2,7 +2,7 @@ module Docs
   class Padrino < UrlScraper
     self.slug = 'padrino'
     self.type = 'rubydoc'
-    self.release = '0.14.1'
+    self.release = '0.14.4'
     self.base_url = 'http://www.rubydoc.info/github/padrino/padrino-framework/'
     self.root_path = 'file/README.rdoc'
     self.initial_paths = %w(index2)
@@ -16,7 +16,7 @@ module Docs
     options[:container] = ->(filter) { filter.root_page? ? '#filecontents' : '#content' }
 
     options[:attribution] = <<-HTML
-      &copy; 2010&ndash;2016 Padrino<br>
+      &copy; 2010&ndash;2019 Padrino<br>
       Licensed under the MIT License.
     HTML
 
