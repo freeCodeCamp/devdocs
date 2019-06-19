675d022f7ce474b474b73ea1d0b2f9c99fc9d972
Andrew Kraut <akraut@gmail.com>
2019-06-19 12:36:08 -0700

    Update Elixir docs (1.8.2)

--- lib/docs/scrapers/elixir.rb
+++ lib/docs/scrapers/elixir.rb
@@ -33,6 +33,19 @@ module Docs
         "https://elixir-lang.org/getting-started/introduction.html" ]
     end
 
+    version '1.8' do
+      self.release = '1.8.2'
+      self.base_urls = [
+        "https://hexdocs.pm/elixir/#{release}/",
+        "https://hexdocs.pm/eex/#{release}/",
+        "https://hexdocs.pm/ex_unit/#{release}/",
+        "https://hexdocs.pm/iex/#{release}/",
+        "https://hexdocs.pm/logger/#{release}/",
+        "https://hexdocs.pm/mix/#{release}/",
+        'https://elixir-lang.org/getting-started/'
+      ]
+    end
+
     version '1.7' do
       self.release = '1.7.3'
       self.base_urls = [
