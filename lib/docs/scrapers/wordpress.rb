934cee4ae2246bd8a888428d7569b323539007f6
Jasper van Merle <jaspervmerle@gmail.com>
2019-08-12 00:44:53 +0200

    wordpress: implement get_latest_version

--- lib/docs/scrapers/wordpress.rb
+++ lib/docs/scrapers/wordpress.rb
@@ -35,5 +35,10 @@ module Docs
       &copy; 2003&ndash;2019 WordPress Foundation<br>
       Licensed under the GNU GPLv2+ License.
     HTML
+
+    def get_latest_version(opts)
+      doc = fetch_doc('https://wordpress.org/download/releases/', opts)
+      doc.at_css('.releases.latest td').content
+    end
   end
 end
