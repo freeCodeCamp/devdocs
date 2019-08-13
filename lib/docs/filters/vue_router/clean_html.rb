b34dbb25f80e15c7e709c524ce3cd149e205acb3
Jasper van Merle <jaspervmerle@gmail.com>
2019-08-13 18:25:57 +0200

    vue-router: fix notes

--- lib/docs/filters/vue_router/clean_html.rb
+++ lib/docs/filters/vue_router/clean_html.rb
@@ -7,6 +7,13 @@ module Docs
         # Remove unneeded elements
         css('.bit-sponsor, .header-anchor').remove
 
+        css('.custom-block').each do |node|
+          node.name = 'blockquote'
+
+          title = node.at_css('.custom-block-title')
+          title.name = 'strong' unless title.nil?
+        end
+
         doc
       end
     end
