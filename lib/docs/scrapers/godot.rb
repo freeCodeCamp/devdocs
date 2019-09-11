04abf810b7f280f5e82286c471b5e84cc50290a1
Jasper van Merle <jaspervmerle@gmail.com>
2019-09-12 01:02:31 +0200

    Update Godot documentation (3.1.1)

--- lib/docs/scrapers/godot.rb
+++ lib/docs/scrapers/godot.rb
@@ -17,15 +17,20 @@ module Docs
     options[:attribution] = ->(filter) do
       if filter.subpath.start_with?('classes')
          <<-HTML
-          &copy; 2014&ndash;2018 Juan Linietsky, Ariel Manzur, Godot Engine contributors<br>
+          &copy; 2014&ndash;2019 Juan Linietsky, Ariel Manzur, Godot Engine contributors<br>
           Licensed under the MIT License.
         HTML
       else
         <<-HTML
-          &copy; 2014&ndash;2018 Juan Linietsky, Ariel Manzur and the Godot community<br>
+          &copy; 2014&ndash;2019 Juan Linietsky, Ariel Manzur and the Godot community<br>
           Licensed under the Creative Commons Attribution Unported License v3.0.
         HTML
       end
+    end
+
+    version '3.1' do
+      self.release = '3.1.1'
+      self.base_url = "http://docs.godotengine.org/en/#{self.version}/"
     end
 
     version '3.0' do
