eda3a7ffb4b12fd3ee46411694f7c63802f16ffe
Jasper van Merle <jaspervmerle@gmail.com>
2019-09-12 01:05:33 +0200

    Update Puppeteer documentation (1.19.0)

--- lib/docs/scrapers/puppeteer.rb
+++ lib/docs/scrapers/puppeteer.rb
@@ -1,7 +1,7 @@
 module Docs
   class Puppeteer < Github
-    self.release = '1.10.0'
-    self.base_url = 'https://github.com/GoogleChrome/puppeteer/blob/v1.8.0/docs/api.md'
+    self.release = '1.19.0'
+    self.base_url = 'https://github.com/GoogleChrome/puppeteer/blob/v1.19.0/docs/api.md'
     self.links = {
       code: 'https://github.com/GoogleChrome/puppeteer'
     }
