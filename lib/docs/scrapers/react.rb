3045fe1729fedb80c8347bcdf4acfd088c9a84f2
Dominik Bułaj <dominik@bulaj.com>
2019-10-02 22:52:27 +0200

    Update react.rb
    Latest React version is 16.10.1 released September 28, 2019, see: https://github.com/facebook/react/blob/master/CHANGELOG.md#16101-september-28-2019

--- lib/docs/scrapers/react.rb
+++ lib/docs/scrapers/react.rb
@@ -2,7 +2,7 @@ module Docs
   class React < UrlScraper
     self.name = 'React'
     self.type = 'simple'
-    self.release = '16.6.0'
+    self.release = '16.10.0'
     self.base_url = 'https://reactjs.org/docs/'
     self.root_path = 'hello-world.html'
     self.links = {
