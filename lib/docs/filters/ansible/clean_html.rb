90f2988ba3226396c8e5115cf8a13b953fea80cd
Jed Fox <git@twopointzero.us>
2020-01-09 17:18:33 -0500

    Fix ansible docs

--- lib/docs/filters/ansible/clean_html.rb
+++ lib/docs/filters/ansible/clean_html.rb
@@ -2,7 +2,7 @@ module Docs
   class Ansible
     class CleanHtmlFilter < Filter
       def call
-        @doc = at_css('.document')
+        @doc = at_css('[itemprop=articleBody]')
 
         css('font').each do |node|
           node.before(node.children).remove
