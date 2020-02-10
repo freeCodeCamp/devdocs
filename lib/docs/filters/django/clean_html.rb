cb790e95abe398e56392441abb56def3f64ab989
Jed Fox <git@twopointzero.us>
2020-02-10 14:46:45 -0500

    [django] Clean up the multi-platform console blocks

--- lib/docs/filters/django/clean_html.rb
+++ lib/docs/filters/django/clean_html.rb
@@ -4,6 +4,14 @@ module Docs
       def call
         @doc = at_css('.yui-g')
 
+        css('.console-block').each do |node|
+          node.css('input', 'label').remove
+          node.css('section').each do |sec|
+            sec.before(sec.children).remove
+          end
+          node.before(node.children).remove
+        end
+
         doc
       end
     end
