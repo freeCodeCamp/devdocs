d9b03591e19276508ce90ce4c06413d7082981f3
Alex Brazier <aejbrazier.apps@gmail.com>
2020-05-02 01:46:59 +0100

    Update react-native docs to 0.62

--- lib/docs/scrapers/react_native.rb
+++ lib/docs/scrapers/react_native.rb
@@ -2,18 +2,18 @@ module Docs
   class ReactNative < UrlScraper
     self.slug = 'react_native'
     self.type = 'react_native'
-    self.release = '0.56'
-    self.base_url = 'https://facebook.github.io/react-native/docs/'
-    self.root_path = 'getting-started.html'
+    self.release = '0.62'
+    self.base_url = 'https://reactnative.dev/docs/'
+    self.root_path = 'getting-started'
     self.links = {
-      home: 'https://facebook.github.io/react-native/',
+      home: 'https://reactnative.dev/',
       code: 'https://github.com/facebook/react-native'
     }
 
     html_filters.push 'react_native/entries', 'react_native/clean_html'
 
     options[:container] = '.docMainWrapper'
-    options[:skip_patterns] = [/\Asample\-/]
+    options[:skip_patterns] = [/\Asample\-/, /\Anext/]
     options[:skip] = %w(
       videos.html
       transforms.html
@@ -27,12 +27,12 @@ module Docs
     }
 
     options[:attribution] = <<-HTML
-      &copy; 2015&ndash;2018 Facebook Inc.<br>
-      Licensed under the Creative Commons Attribution 4.0 International Public License.
+      &copy; 2015&ndash;2020 Facebook Inc.<br>
+      Licensed under the MIT License.
     HTML
 
     def get_latest_version(opts)
-      doc = fetch_doc('https://facebook.github.io/react-native/docs/getting-started.html', opts)
+      doc = fetch_doc('https://reactnative.dev/docs/getting-started', opts)
       doc.at_css('header > a > h3').content
     end
   end
