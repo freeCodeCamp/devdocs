c39f1a138b5056c6c43bc1972be8b0fa0ce80e38
Antoine Pietri <antoine.pietri1@gmail.com>
2020-05-24 14:29:08 +0200

    ocaml: enable highlighting of caml-example code fragments

--- lib/docs/filters/ocaml/clean_html.rb
+++ lib/docs/filters/ocaml/clean_html.rb
@@ -6,6 +6,17 @@ module Docs
           node['data-language'] = 'ocaml'
         end
 
+        css('.caml-input').each do |node|
+          node.content = '# ' + node.content.strip
+        end
+
+        css('.caml-example').each do |node|
+          node.name = 'pre'
+          node.traverse { |n| n.remove if n.text? && n.text !~ /\S/ }
+
+          node['data-language'] = 'ocaml'
+        end
+
         doc
       end
     end
