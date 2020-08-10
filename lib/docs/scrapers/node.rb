b40b8af0d8829091da158bf2f6d36c52dc4cdcc4
Jiawen Geng <technicalcute@gmail.com>
2020-08-10 03:47:19 +0000

    add Node.js 14

--- lib/docs/scrapers/node.rb
+++ lib/docs/scrapers/node.rb
@@ -23,7 +23,12 @@ module Docs
     HTML
 
     version do
-      self.release = '12.9.1'
+      self.release = '14.7.0'
+      self.base_url = 'https://nodejs.org/dist/latest-v14.x/docs/api/'
+    end
+
+    version '12 LTS' do
+      self.release = '12.18.3'
       self.base_url = 'https://nodejs.org/dist/latest-v12.x/docs/api/'
     end
 
