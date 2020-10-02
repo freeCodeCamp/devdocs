c037839b5a88ef39eef00a9e18d203857d384ab2
Dominik Bułaj <dominik@bulaj.com>
2020-10-02 10:09:31 +0200

    Update react.rb
    Updated react version to current latest one (16.13.1) https://reactjs.org/versions

--- lib/docs/scrapers/react.rb
+++ lib/docs/scrapers/react.rb
@@ -2,7 +2,7 @@ module Docs
   class React < UrlScraper
     self.name = 'React'
     self.type = 'simple'
-    self.release = '16.12.0'
+    self.release = '16.13.1'
     self.base_url = 'https://reactjs.org/docs/'
     self.root_path = 'hello-world.html'
     self.links = {
