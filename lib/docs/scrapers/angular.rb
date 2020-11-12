5c9f26e0d669ece0ad8d6f4d39572f12fe0740f5
Simon Legner <Simon.Legner@gmail.com>
2020-11-12 21:27:43 +0100

    angular: add version 10

--- lib/docs/scrapers/angular.rb
+++ lib/docs/scrapers/angular.rb
@@ -59,6 +59,26 @@ module Docs
     end
 
     version do
+      self.release = '10.2.3'
+      self.base_url = 'https://v10.angular.io/'
+      self.root_path = 'docs'
+
+      html_filters.push 'angular/clean_html', 'angular/entries'
+
+      options[:follow_links] = false
+      options[:only_patterns] = [/\Aguide/, /\Atutorial/, /\Aapi/]
+      options[:fix_urls_before_parse] = ->(url) do
+        url.sub! %r{\Aguide/}, '/guide/'
+        url.sub! %r{\Atutorial/}, '/tutorial/'
+        url.sub! %r{\Aapi/}, '/api/'
+        url.sub! %r{\Agenerated/}, '/generated/'
+        url
+      end
+
+      include Docs::Angular::Common
+    end
+
+    version '9' do
       self.release = '9.1.12'
       self.base_url = 'https://v9.angular.io/'
       self.root_path = 'docs'
