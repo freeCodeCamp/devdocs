26d1a1f06d96c3530303d7bb9544b39f29cc1433
samfundev <samdinner@hotmail.com>
2020-11-18 21:23:36 -0500

    codeception: update to 4.1.12

--- lib/docs/scrapers/codeception.rb
+++ lib/docs/scrapers/codeception.rb
@@ -2,7 +2,7 @@ module Docs
   class Codeception < UrlScraper
     self.name = 'Codeception'
     self.type = 'codeception'
-    self.release = '3.0.3'
+    self.release = '4.1.12'
     self.base_url = 'https://codeception.com/docs/'
     self.root_path = 'index.html'
     self.links = {
