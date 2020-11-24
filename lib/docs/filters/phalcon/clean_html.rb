01707223a9cb2059271c11c50df48ed6792fd93d
MasterEnoc <brianhernandez222@hotmail.com>
2020-11-24 15:22:25 -0600

    Update Phalcon version 3 and add Phalcon version 4
    - Delete Phalcon version 2 since pages of its documentations
      are not longer available

--- lib/docs/filters/phalcon/clean_html.rb
+++ lib/docs/filters/phalcon/clean_html.rb
@@ -2,45 +2,24 @@ module Docs
   class Phalcon
     class CleanHtmlFilter < Filter
       def call
-        @doc = at_css('.body')
 
-        if root_page?
-          at_css('h1').content = 'Phalcon'
-        end
+        # left navigation bar
+        css('.doc-article-nav-wr').remove
 
-        css('#what-is-phalcon', '#other-formats').remove
+        # right navigation bar
+        css('.phalcon-blog__right-item').remove
 
-        css('#methods > p > strong, #constants > p > strong').each do |node|
-          node.parent.name = 'h3'
-          node.parent['id'] = node.content.parameterize
-          node.parent['class'] = 'method-signature'
-          node.parent.inner_html = node.parent.inner_html.sub(/inherited from .*/, '<small>\0</small>')
-        end
+        css('header').remove
 
-        css('.headerlink').each do |node|
-          id = node['href'][1..-1]
-          node.parent['id'] ||= id
-          node.remove
-        end
+        css('footer').remove
 
-        css('div[class^="highlight-"]').each do |node|
-          code = node.at_css('pre').content
-          code.remove! %r{\A\s*<\?php\s*} unless code.include?(' ?>')
-          node.content = code
-          node.name = 'pre'
-          node['data-language'] = node['class'][/highlight-(\w+)/, 1]
-          node['data-language'] = 'php' if node['data-language'] == 'html+php'
-        end
-
-        css('.section').each do |node|
-          node.before(node.children).remove
-        end
-
-        css('table[border]').each do |node|
-          node.remove_attribute('border')
+        # initial table of contents
+        if !(slug=='index')
+          css('.docSearch-content > ul').remove
         end
 
         doc
+
       end
     end
   end
