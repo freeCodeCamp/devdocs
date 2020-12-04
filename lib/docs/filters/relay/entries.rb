e9322e467eed46935b50969923e4960778d472ca
MasterEnoc <brianhernandez222@hotmail.com>
2020-12-03 22:07:29 -0600

    Update Relay to 10.1.0

--- lib/docs/filters/relay/entries.rb
+++ lib/docs/filters/relay/entries.rb
@@ -1,43 +1,53 @@
 module Docs
   class Relay
     class EntriesFilter < Docs::EntriesFilter
+
       def get_name
-        at_css('h1').children.select(&:text?).map(&:content).join.strip
+        if slug == 'index'
+          return 'Relay'
+        end
+
+        at_css('h1').content
       end
 
       def get_type
-        link = at_css('.nav-docs-section .active, .toc .active')
-        section = link.ancestors('.nav-docs-section, section').first
-        type = section.at_css('h3').content.strip
-        type
+        if slug == 'index'
+          return 'Relay'
+        end
+
+        at_css('h1').content
       end
 
       def additional_entries
         entries = []
 
-        css('.inner-content h3 code, .inner-content h4 code').each do |node|
-          name = node.content
-          name.remove! %r{[#\(\)]}
-          name.remove! %r{\w+\:}
-          name.strip!
-          id = name.parameterize
-          node.parent['id'] = id
-          entries << [name, id, 'Reference']
+        if slug == 'index'
+          return entries
         end
 
-        css('.apiIndex a pre').each do |node|
-          next unless node.parent['href'].start_with?('#')
-          id = node.parent['href'].remove('#')
-          name = node.content.strip
-          sep = name.start_with?('static') ? '.' : '#'
-          name.remove! %r{(abstract|static) }
-          name.sub! %r{\(.*\)}, '()'
-          name.prepend(self.name + sep)
-          entries << [name, id]
+        ## avoid adding non-desired entries removing tags
+        # remove header which contains a <h2> tag
+        css('.fixedHeaderContainer').remove
+
+        # remove table of content whose title is an <h2> tag
+        css('.toc').remove
+        ##
+
+        css('h2, h3').each do |node|
+          next if node.content.include?('Argument')
+          entry_name = node.content
+
+          if entry_name.include?('(')
+            entry_name = entry_name.match(/.*\(/)[0] + ')'
+          end
+
+          entry_id = node.content.gsub(/\s/, '-').downcase
+          entries << [entry_name, entry_id]
         end
 
         entries
       end
+
     end
   end
 end
