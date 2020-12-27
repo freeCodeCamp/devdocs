0c01cbf759f38dd3d1ed71ff8c1c9f0182970e4a
Simon Legner <Simon.Legner@gmail.com>
2020-12-27 19:12:57 +0100

    Update npm documentation (7.3.0)

--- lib/docs/scrapers/npm.rb
+++ lib/docs/scrapers/npm.rb
@@ -2,7 +2,7 @@ module Docs
   class Npm < UrlScraper
     self.name = 'npm'
     self.type = 'npm'
-    self.release = '7.1.2'
+    self.release = '7.3.0'
     self.base_url = 'https://docs.npmjs.com/'
     self.force_gzip = true
     self.links = {
