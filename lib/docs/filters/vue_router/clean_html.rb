b724e81d2f0d0bf03a48d31f973157e92c6da97c
Simon Legner <Simon.Legner@gmail.com>
2021-01-30 00:38:53 +0100

    Update Vue Router documentation (3.5.1 and 4.0.3)

--- lib/docs/filters/vue_router/clean_html.rb
+++ lib/docs/filters/vue_router/clean_html.rb
@@ -2,7 +2,7 @@ module Docs
   class VueRouter
     class CleanHtmlFilter < Filter
       def call
-        @doc = at_css('.content')
+        @doc = at_css('main')
 
         # Remove unneeded elements
         css('.bit-sponsor, .header-anchor').remove
@@ -12,6 +12,13 @@ module Docs
 
           title = node.at_css('.custom-block-title')
           title.name = 'strong' unless title.nil?
+        end
+
+        # Remove data-v-* attributes
+        css('*').each do |node|
+          node.attributes.each_key do |attribute|
+            node.remove_attribute(attribute) if attribute.start_with? 'data-v-'
+          end
         end
 
         doc
--- lib/docs/scrapers/vue_router.rb
+++ lib/docs/scrapers/vue_router.rb
@@ -3,8 +3,6 @@ module Docs
     self.name = 'Vue Router'
     self.slug = 'vue_router'
     self.type = 'simple'
-    self.release = '3.1.2'
-    self.base_url = 'https://router.vuejs.org/'
     self.links = {
       home: 'https://router.vuejs.org',
       code: 'https://github.com/vuejs/vue-router'
@@ -22,8 +20,18 @@ module Docs
       Licensed under the MIT License.
     HTML
 
+    version '4' do
+      self.release = '4.0.3'
+      self.base_url = 'https://next.router.vuejs.org/'
+    end
+
+    version '3' do
+      self.release = '3.5.1'
+      self.base_url = 'https://router.vuejs.org/'
+    end
+
     def get_latest_version(opts)
-      get_latest_github_release('vuejs', 'vue-router', opts)
+      get_npm_version('vue-router', opts, 'next')
     end
   end
 end
