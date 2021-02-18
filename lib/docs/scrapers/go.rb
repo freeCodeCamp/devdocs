42803937f4b0ba10f7234b2c018d1bc8cbdcc056
Simon Legner <Simon.Legner@gmail.com>
2021-02-18 18:13:00 +0100

    Update Go documentation (1.16)

--- lib/docs/scrapers/go.rb
+++ lib/docs/scrapers/go.rb
@@ -1,7 +1,7 @@
 module Docs
   class Go < UrlScraper
     self.type = 'go'
-    self.release = '1.15'
+    self.release = '1.16'
     self.base_url = 'https://golang.org/pkg/'
     self.links = {
       home: 'https://golang.org/',
