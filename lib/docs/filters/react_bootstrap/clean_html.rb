306b264d763ce96d9ef47c2305f54727cc6f2214
dohsimpson <mydohsimpson@gmail.com>
2021-02-23 00:53:29 -0500

    Remove unnecessary classes

--- lib/docs/filters/react_bootstrap/clean_html.rb
+++ lib/docs/filters/react_bootstrap/clean_html.rb
@@ -12,6 +12,14 @@ module Docs
           node['data-language'] = 'jsx'
           node.parent.replace(node)
         end
+
+        css('div, main, pre, h1, h2, h3, h4, h5, h6, a, p').each do |node|
+          node.delete 'class'
+        end
+
+        css('#___gatsby, #gatsby-focus-wrapper').each do |node|
+          node.delete 'id'
+        end
         doc
       end
     end
