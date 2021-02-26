e14b89164693f2038ce4567adf0d7aa3e4ec2b53
Damilola Olowookere <damms005@gmail.com>
2021-02-26 18:00:02 +0100

    First community review version for Tailwind CSS

--- lib/docs/filters/tailwindcss/clean_html.rb
+++ lib/docs/filters/tailwindcss/clean_html.rb
@@ -2,75 +2,17 @@ module Docs
   class Tailwindcss
     class CleanHtmlFilter < Filter
       def call
-          clean_up
+        css('div.sticky.top-0').remove
+        css('.sr-only').remove
+        css('#sidebar').remove
 
-        # Remove code highlighting
-        css('pre').each do |node|
-          node.content = node.content
-          node['data-language'] = 'php'
-        end
-
-        doc
-      end
-
-      def clean_up
-        @doc = doc.at_css('#content-wrapper')
-
-        css('.location').remove
-
-        # Replace .header with <h1>
-        css('.page-header > h1').each do |node|
-          node.content = 'Tailwind' if root_page?
-          node.parent.before(node).remove
-        end
-
-        css('.container-fluid').each do |node|
-          node.name = 'table'
-          node.css('.row').each { |n| n.name = 'tr' }
-          node.css('div[class^="col"]').each { |n| n.name = 'td' }
-        end
-
-        css('> div').each do |node|
-          node.before(node.children).remove
-        end
-
-        # Remove <abbr>
-        css('a > abbr').each do |node|
-          node.parent['title'] = node['title']
-          node.before(node.children).remove
-        end
-
-        # Clean up headings
-        css('h1 > a', '.content', 'h3 > code', 'h3 strong', 'abbr').each do |node|
-          node.before(node.children).remove
-        end
-
-        # Remove empty <td>
-        css('td').each do |node|
-          node.remove if node.content =~ /\A\s+\z/
-        end
-
-        # @doc = at_css('.docs_body')
-
-        # Clean up headings
-        css('h2 > a').each do |node|
-          node.before(node.children).remove
-        end
-
-        css('p > a[name]').each do |node|
-          node.parent.next_element['id'] = node['name']
-        end
-
-        css('blockquote').each do |node|
-          node['class'] = 'tip' if node.inner_html.include?('{tip}')
-          node.inner_html = node.inner_html.remove(/\{(tip|note)\}\s?/)
-        end
-
-        css('blockquote').each do |node|
-          if node.inner_html.include?('You\'re browsing the documentation for an old version of Laravel.')
-            node.remove
+        css('#nav ul li').each do |node|
+          link = node.css("a").attr('href').to_s
+          if link.include? "https://"
+            node.remove()
           end
         end
+        doc
       end
     end
   end
--- lib/docs/scrapers/tailwindcss.rb
+++ lib/docs/scrapers/tailwindcss.rb
@@ -4,16 +4,23 @@ module Docs
     self.type = 'tailwindcss'
     self.slug = 'tailwindcss'
     self.base_url = 'https://tailwindcss.com/docs'
-    self.root_path = '/installation'
-    self.release = '8.5'
+    self.root_path = '/'
+    self.release = '2.0.3'
 
     html_filters.push 'tailwindcss/entries', 'tailwindcss/clean_html'
 
     # options[:container] = 'body';
 
     options[:skip_patterns] = [
+
+        # removed because it focuses on how to use Tailwind with some other niche
+        # technologies and to also align with DevDoc's Vision, which is" to:"
+        # "indexing only the minimum useful to most developers" that use Tailwind
         %r{\/guides\/.*},
+
+        # removed so it is easy to "get_type" (see tailwindcss/entries.rb line #15)
         %r{\/colors\z}
+
       ]
 
     #Obtainable from https://github.com/tailwindlabs/tailwindcss/blob/master/LICENSE
@@ -25,7 +32,7 @@ module Docs
 
     def get_latest_version(opts)
       doc = fetch_doc('https://tailwindcss.com/docs/installation', opts)
-      doc.at_css('select option[value=v2]').inner_text
+      doc.at_css('select option[value=v2]').inner_text[1..]
     end
   end
 end
