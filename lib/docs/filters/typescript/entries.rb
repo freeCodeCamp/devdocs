91138ad50894e0ba2159bf7f0192f33ae426d83f
Simon Legner <Simon.Legner@gmail.com>
2021-03-06 00:37:42 +0100

    Update TypeScript documentation (New Handbook)
    https://devblogs.microsoft.com/typescript/announcing-the-new-typescript-handbook/

--- lib/docs/filters/typescript/entries.rb
+++ lib/docs/filters/typescript/entries.rb
@@ -2,15 +2,31 @@ module Docs
   class Typescript
     class EntriesFilter < Docs::EntriesFilter
 
+      DEPRECATED_PAGES = %w(
+        advanced-types
+        basic-types
+        classes
+        functions
+        generics
+        interfaces
+        literal-types
+        unions-and-intersections
+      )
+
       def get_name
         at_css('h1') ? at_css('h1').content : at_css('h2').content
       end
 
       def get_type
-        name
+        if DEPRECATED_PAGES.include? slug
+          'Handbook (deprecated)'
+        else
+          name
+        end
       end
 
       def additional_entries
+        return [] if DEPRECATED_PAGES.include? slug
         base_url.path == '/' ? tsconfig_entries : handbook_entries
       end
 
