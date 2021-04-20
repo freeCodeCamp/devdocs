0ad6fefa83e4818ac1a170239acb1d6a3e690005
Enoc <brianhernandez222@hotmail.com>
2021-04-20 01:46:30 -0600

    Add support for compatibility tables
    almost all the mdn docs works, but some have
    minor issues.

--- lib/docs/filters/svg/clean_html.rb
+++ lib/docs/filters/svg/clean_html.rb
@@ -7,7 +7,7 @@ module Docs
       end
 
       def root
-        doc.inner_html = doc.at_css('#Documentation + dl').to_html
+
       end
 
       def other
@@ -26,7 +26,20 @@ module Docs
           node['class'] = 'index'
           css('h3').each { |n| n.name = 'span' }
         end
+
+        if at_css('#browser_compatibility') \
+          and not at_css('#browser_compatibility').next_sibling.classes.include?('warning') \
+          and not at_css('#browser_compatibility').next_sibling.content.match?('Supported')
+
+          at_css('#browser_compatibility').next_sibling.remove
+
+          compatibility_tables = generate_compatibility_table()
+          compatibility_tables.each do |table|
+            at_css('#browser_compatibility').add_next_sibling(table)
+          end
+        end
       end
+
     end
   end
 end
--- lib/docs/filters/xslt_xpath/clean_html.rb
+++ lib/docs/filters/xslt_xpath/clean_html.rb
@@ -24,6 +24,19 @@ module Docs
           child = child.next while child && child.text? && child.content.blank?
           child.remove if child.try(:name) == 'br'
         end
+
+        if at_css('#browser_compatibility') \
+          and not at_css('#browser_compatibility').next_sibling.classes.include?('warning') \
+          and not at_css('#browser_compatibility').next_sibling.content.match?('Supported')
+
+          at_css('#browser_compatibility').next_sibling.remove
+
+          compatibility_tables = generate_compatibility_table()
+          compatibility_tables.each do |table|
+            at_css('#browser_compatibility').add_next_sibling(table)
+          end
+        end
+
       end
     end
   end
