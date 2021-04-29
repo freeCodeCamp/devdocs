cd6664a4601d2248239d5ba0f27bb1e7721cdf8f
Simon Legner <Simon.Legner@gmail.com>
2021-04-29 22:59:22 +0200

    Update GNU Fortran documentation (11.1.0)

--- lib/docs/scrapers/gnu/gnu_fortran.rb
+++ lib/docs/scrapers/gnu/gnu_fortran.rb
@@ -6,6 +6,11 @@ module Docs
       home: 'https://gcc.gnu.org/fortran/'
     }
 
+    version '11' do
+      self.release = '11.1.0'
+      self.base_url = "https://gcc.gnu.org/onlinedocs/gcc-#{release}/gfortran/"
+    end
+
     version '10' do
       self.release = '10.2.0'
       self.base_url = "https://gcc.gnu.org/onlinedocs/gcc-#{release}/gfortran/"
