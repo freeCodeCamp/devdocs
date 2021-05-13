38660c534a904fb380137b418eb8a690680119c6
Simon Legner <Simon.Legner@gmail.com>
2021-05-13 12:28:05 +0200

    Update D3.js documentation (6.7.0)

--- lib/docs/scrapers/d3.rb
+++ lib/docs/scrapers/d3.rb
@@ -17,7 +17,7 @@ module Docs
     HTML
 
     version '6' do
-      self.release = '6.6.0'
+      self.release = '6.7.0'
       self.base_url = 'https://github.com/d3/'
       self.root_path = 'd3/blob/master/API.md'
 
