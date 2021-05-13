b5635fa550c328299b557cb70fc5d2996f19dc06
Simon Legner <Simon.Legner@gmail.com>
2021-05-13 13:19:55 +0200

    Update Angular documentation (12.0.0)

--- lib/docs/scrapers/angular.rb
+++ lib/docs/scrapers/angular.rb
@@ -59,8 +59,28 @@ module Docs
     end
 
     version do
-      self.release = '11.2.6'
+      self.release = '12.0.0'
       self.base_url = 'https://angular.io/'
+      self.root_path = 'docs'
+
+      html_filters.push 'angular/clean_html', 'angular/entries'
+
+      options[:follow_links] = false
+      options[:only_patterns] = [/\Aguide/, /\Atutorial/, /\Aapi/]
+      options[:fix_urls_before_parse] = ->(url) do
+        url.sub! %r{\Aguide/}, '/guide/'
+        url.sub! %r{\Atutorial/}, '/tutorial/'
+        url.sub! %r{\Aapi/}, '/api/'
+        url.sub! %r{\Agenerated/}, '/generated/'
+        url
+      end
+
+      include Docs::Angular::Common
+    end
+
+    version '11' do
+      self.release = '11.2.14'
+      self.base_url = 'https://v11.angular.io/'
       self.root_path = 'docs'
 
       html_filters.push 'angular/clean_html', 'angular/entries'
