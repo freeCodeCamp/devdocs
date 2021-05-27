7269d5431fef4748dd248e70a74a74d33ade63fb
Cimbali <me@cimba.li>
2021-05-27 14:09:21 +0200

    Improve jq examples: simplify HTML, add %pre CSS

--- lib/docs/filters/jq/clean_html.rb
+++ lib/docs/filters/jq/clean_html.rb
@@ -2,7 +2,19 @@ module Docs
   class Jq
     class CleanHtmlFilter < Filter
       def call
-        at_css('div#manualcontent')
+        content = at_css('div#manualcontent')
+
+        css('.manual-example').each do |node|
+          container = node.parent
+          example_header = doc.document.create_element('h4')
+          example_header.content = container.at_css('a[data-toggle="collapse"]').content
+          node.children.before(example_header)
+
+          node.remove_class('collapse')
+          container.replace(node)
+        end
+
+        content
       end
     end
   end
