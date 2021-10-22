45027d5a29cada751583e1c5c5056e7e6d820f08
Simon Legner <Simon.Legner@gmail.com>
2021-10-22 19:28:42 +0200

    Update CSS documentation

--- lib/docs/scrapers/mdn/css.rb
+++ lib/docs/scrapers/mdn/css.rb
@@ -1,5 +1,6 @@
 module Docs
   class Css < Mdn
+    # release = '2021-10-22'
     self.name = 'CSS'
     self.base_url = 'https://developer.mozilla.org/en-US/docs/Web/CSS'
     self.root_path = '/Reference'
