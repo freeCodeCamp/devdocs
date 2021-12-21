82890a43b80f38d4eef18086b34ca13283742bb2
Simon Legner <Simon.Legner@gmail.com>
2021-12-22 00:19:16 +0100

    Update HTML documentation

--- lib/docs/scrapers/mdn/html.rb
+++ lib/docs/scrapers/mdn/html.rb
@@ -2,7 +2,7 @@ module Docs
   class Html < Mdn
     prepend FixInternalUrlsBehavior
 
-    # release = '2021-10-22'
+    # release = '2021-12-21'
     self.name = 'HTML'
     self.base_url = 'https://developer.mozilla.org/en-US/docs/Web/HTML'
 
