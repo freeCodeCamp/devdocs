20445fd37e5cc4980bcced7b9c388f3e31a44bc2
Aditya Ardiya <aditya.ardiya@gmail.com>
2022-01-16 02:12:01 +0900

    Improve point_cloud_library clean_html

--- lib/docs/filters/point_cloud_library/clean_html.rb
+++ lib/docs/filters/point_cloud_library/clean_html.rb
@@ -5,6 +5,18 @@ module Docs
         @doc = at_css('.contents')
         css('.dynheader.closed').remove
         css('.permalink').remove
+        css('.memSeparator').remove
+
+        # Change div.fragment to C++ code with syntax highlight
+        css('div.fragment').each do |node|
+          node.name = 'pre'
+          node['data-language'] = 'cpp'
+          node_content = ""
+          node.css('div').each do |inner_node|
+            node_content += inner_node.text + "\n"
+          end
+          node.content = node_content
+        end
         doc
       end
     end
