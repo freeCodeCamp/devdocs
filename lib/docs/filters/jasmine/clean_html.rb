74352f805a1f2675a75d9f1f5b5524b374f9afb4
Enoc <brianhernandez222@hotmail.com>
2022-02-10 19:22:40 -0600

    Remove comments in Jasmine's clean_html filter

--- lib/docs/filters/jasmine/clean_html.rb
+++ lib/docs/filters/jasmine/clean_html.rb
@@ -4,18 +4,6 @@ module Docs
       def call
         @doc = at_css('.docs') unless root_page?
 
-        # css('header', 'article', 'section:not([class])', 'div.description').each do |node|
-        #   node.before(node.children).remove
-        # end
-
-        # css('h3.subsection-title').each do |node|
-        #   node.name = 'h2'
-        # end
-
-        # css('h4.name').each do |node|
-        #   node.name = 'h3'
-        # end
-
         css('pre').each do |node|
           node.content = node.content
           node['data-language'] = 'javascript'
