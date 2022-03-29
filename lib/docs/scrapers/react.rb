44d42bbd5c13f728d081a2cd1c3b0052b223ab3f
Simon Legner <Simon.Legner@gmail.com>
2022-03-29 23:01:16 +0200

    Update React documentation (18.0.0)

--- lib/docs/scrapers/react.rb
+++ lib/docs/scrapers/react.rb
@@ -2,7 +2,7 @@ module Docs
   class React < UrlScraper
     self.name = 'React'
     self.type = 'simple'
-    self.release = '17.0.2'
+    self.release = '18.0.0'
     self.base_url = 'https://reactjs.org/docs/'
     self.root_path = 'hello-world.html'
     self.links = {
