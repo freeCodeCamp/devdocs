abbb746936a1cf8ceed221fc99cdb53b7ca37191
Simon Legner <Simon.Legner@gmail.com>
2022-04-22 21:08:54 +0200

    Update Deno documentation (1.21.0)

--- lib/docs/scrapers/deno.rb
+++ lib/docs/scrapers/deno.rb
@@ -2,7 +2,7 @@ module Docs
   class Deno < UrlScraper
     self.name = 'Deno'
     self.type = 'simple'
-    self.release = '1.20.0'
+    self.release = '1.21.0'
     self.base_url = 'https://doc.deno.land/deno/stable/'
     self.links = {
       home: 'https://deno.land/',
