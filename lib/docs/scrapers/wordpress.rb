1f1e1446932632ba359b6f159ecddda477e50b9a
Simon Legner <Simon.Legner@gmail.com>
2022-05-25 21:17:18 +0200

    Update WordPress documentation (6.0)

--- lib/docs/scrapers/wordpress.rb
+++ lib/docs/scrapers/wordpress.rb
@@ -2,7 +2,7 @@ module Docs
   class Wordpress < UrlScraper
     self.name = 'WordPress'
     self.type = 'wordpress'
-    self.release = '5.9'
+    self.release = '6.0'
     self.base_url = 'https://developer.wordpress.org/reference/'
     self.initial_paths = %w(
       functions/
