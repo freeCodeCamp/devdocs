77f4a47b14cdf02073e5adf368c729f2cb5221c9
Simon Legner <Simon.Legner@gmail.com>
2022-06-02 22:53:59 +0200

    Update Clojure documentation (1.11)

--- lib/docs/scrapers/clojure.rb
+++ lib/docs/scrapers/clojure.rb
@@ -17,9 +17,14 @@ module Docs
       Licensed under the Eclipse Public License 1.0.
     HTML
 
-    version '1.10' do
-      self.release = '1.10 (stable)'
+    version '1.11' do
+      self.release = '1.11'
       self.base_url = 'https://clojure.github.io/clojure/'
+    end
+
+    version '1.10' do
+      self.release = '1.10.3'
+      self.base_url = "https://clojure.github.io/clojure/branch-clojure-#{self.release}/"
     end
 
     version '1.9' do
