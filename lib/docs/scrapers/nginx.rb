95cc9a79ea496241ed0bdbd6bfa9ee6b967af06a
Simon Legner <Simon.Legner@gmail.com>
2022-07-31 09:11:14 +0200

    Update nginx documentation (1.23.1)

--- lib/docs/scrapers/nginx.rb
+++ lib/docs/scrapers/nginx.rb
@@ -2,7 +2,7 @@ module Docs
   class Nginx < UrlScraper
     self.name = 'nginx'
     self.type = 'nginx'
-    self.release = '1.22.0'
+    self.release = '1.23.1'
     self.base_url = 'https://nginx.org/en/docs/'
     self.links = {
       home: 'https://nginx.org/',
