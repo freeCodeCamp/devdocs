150c56e0ced82a6b8d01400bf050bd93db50fcea
Simon Legner <Simon.Legner@gmail.com>
2022-09-27 22:09:40 +0200

    Update Kubectl documentation (1.25)

--- lib/docs/scrapers/kubectl.rb
+++ lib/docs/scrapers/kubectl.rb
@@ -21,7 +21,7 @@ module Docs
 
     # latest version has a special URL that does not include the version identifier 
     version do
-      self.release = "1.23"
+      self.release = "1.25"
       self.base_url = "https://kubernetes.io/docs/reference/generated/kubectl/kubectl-commands"
     end
 
