40ade81c60db7f29117246a79eb765dc9f0c605f
Simon Legner <Simon.Legner@gmail.com>
2022-11-16 22:38:39 +0100

    axios: remove code highlighting

--- lib/docs/filters/axios/clean_html.rb
+++ lib/docs/filters/axios/clean_html.rb
@@ -3,6 +3,10 @@ module Docs
     class CleanHtmlFilter < Filter
       def call
         css('.links').remove
+        css('pre').each do |node|
+          node.content = node.content
+          node['data-language'] = node['class'][/lang-(\w+)/, 1]
+        end
         doc
       end
     end
