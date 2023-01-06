03d2d650784051ab2fe9a65059479af14120fcf1
Simon Legner <Simon.Legner@gmail.com>
2023-01-06 21:52:36 +0100

    Update Web APIs documentation

--- lib/docs/scrapers/mdn/dom.rb
+++ lib/docs/scrapers/mdn/dom.rb
@@ -1,7 +1,7 @@
 module Docs
   class Dom < Mdn
 
-    # release = '2022-04-27'
+    # release = '2023-01-06'
     self.name = 'Web APIs'
     self.slug = 'dom'
     self.base_url = 'https://developer.mozilla.org/en-US/docs/Web/API'
