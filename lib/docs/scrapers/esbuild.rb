b06498442ff0780f0620e26dca95fe6e61a6f9e6
Simon Legner <Simon.Legner@gmail.com>
2023-01-18 00:09:40 +0100

    Update esbuild documentation (0.17.2)

--- lib/docs/scrapers/esbuild.rb
+++ lib/docs/scrapers/esbuild.rb
@@ -16,7 +16,7 @@ module Docs
       Licensed under the MIT License.
     HTML
 
-    self.release = '0.16.10'
+    self.release = '0.17.2'
     self.base_url = 'https://esbuild.github.io/'
     html_filters.push 'esbuild/clean_html', 'esbuild/entries'
 
