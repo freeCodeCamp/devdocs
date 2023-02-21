b85f071466a54f7d48dfde3188fb17c0b94df80c
Simon Legner <Simon.Legner@gmail.com>
2023-02-21 19:09:51 +0100

    Update Angular documentation (15.1.5)

--- lib/docs/scrapers/angular.rb
+++ lib/docs/scrapers/angular.rb
@@ -83,7 +83,7 @@ module Docs
     end
 
     version do
-      self.release = '15.1.4'
+      self.release = '15.1.5'
       self.base_url = 'https://angular.io/'
       include Docs::Angular::Since12
     end
@@ -95,13 +95,13 @@ module Docs
     end
 
     version '13' do
-      self.release = '13.3.12'
+      self.release = '13.3.8'
       self.base_url = 'https://v13.angular.io/'
       include Docs::Angular::Since12
     end
 
     version '12' do
-      self.release = '12.2.17'
+      self.release = '12.2.13'
       self.base_url = 'https://v12.angular.io/'
       include Docs::Angular::Since12
     end
@@ -113,13 +113,13 @@ module Docs
     end
 
     version '10' do
-      self.release = '10.2.5'
+      self.release = '10.2.3'
       self.base_url = 'https://v10.angular.io/'
       include Docs::Angular::Common
     end
 
     version '9' do
-      self.release = '9.1.13'
+      self.release = '9.1.12'
       self.base_url = 'https://v9.angular.io/'
       include Docs::Angular::Common
     end
@@ -131,7 +131,7 @@ module Docs
     end
 
     version '7' do
-      self.release = '7.2.16'
+      self.release = '7.2.15'
       self.base_url = 'https://v7.angular.io/'
       include Docs::Angular::Common
     end
@@ -149,7 +149,7 @@ module Docs
     end
 
     version '4' do
-      self.release = '4.4.7'
+      self.release = '4.4.6'
       self.base_url = 'https://v4.angular.io/'
       include Docs::Angular::Common
     end
