e8dcd14cdfefe891feb06b997c7a58dd4b78d35f
Simon Legner <Simon.Legner@gmail.com>
2023-04-12 22:30:10 +0200

    Update Octave documentation (8.1.0)

--- lib/docs/scrapers/octave.rb
+++ lib/docs/scrapers/octave.rb
@@ -23,11 +23,16 @@ module Docs
     options[:root_title] = 'GNU Octave'
 
     options[:attribution] = <<-HTML
-      &copy; 1996–2022 The Octave Project Developers<br>
+      &copy; 1996–2023 The Octave Project Developers<br>
       Permission is granted to make and distribute verbatim copies of this manual provided the copyright notice and this permission notice are preserved on all copies.<br/>
       Permission is granted to copy and distribute modified versions of this manual under the conditions for verbatim copying, provided that the entire resulting derived work is distributed under the terms of a permission notice identical to this one.</br>
       Permission is granted to copy and distribute translations of this manual into another language, under the above conditions for modified versions.
     HTML
+
+    version '8' do
+      self.release = '8.1.0'
+      self.base_url = "https://docs.octave.org/v#{self.release}/"
+    end
 
     version '7' do
       self.release = '7.2.0'
