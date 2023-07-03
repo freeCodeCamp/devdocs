2e3673fe40889766e39584182e02716f7e25e540
Simon Legner <Simon.Legner@gmail.com>
2023-07-03 21:10:28 +0200

    Update React Router documentation (6.14.1)

--- lib/docs/scrapers/react_router.rb
+++ lib/docs/scrapers/react_router.rb
@@ -3,8 +3,9 @@ module Docs
     self.name = 'React Router'
     self.slug = 'react_router'
     self.type = 'simple'
-    self.release = '6.10.0'
-    self.base_url = 'https://reactrouter.com/en/6.10.0/start/overview'
+    self.release = '6.14.1'
+    self.base_url = 'https://reactrouter.com/en/6.14.1/'
+    self.initial_paths = %w(start/overview)
 
     self.links = {
       home: 'https://reactrouter.com/',
