da343eca8088eb992edffdc40408876592eb8be2
Simon Legner <Simon.Legner@gmail.com>
2023-07-26 20:38:23 +0200

    Update Mongoose documentation (7.4.1)
    Fixes #2012.

--- lib/docs/scrapers/mongoose.rb
+++ lib/docs/scrapers/mongoose.rb
@@ -2,7 +2,7 @@ module Docs
   class Mongoose < UrlScraper
     self.name = 'Mongoose'
     self.type = 'simple'
-    self.release = '5.8.5'
+    self.release = '7.4.1'
     self.base_url = 'https://mongoosejs.com/docs/'
     self.root_path = 'index.html'
     self.initial_paths = %w(guide.html)
