128ea741e754eafa4f3162e97ad722843db7a968
Vedant 17 <98684243+17Ved@users.noreply.github.com>
2023-09-16 23:26:17 +0530

    Fixing the issue

--- lib/docs/scrapers/cppref/c.rb
+++ lib/docs/scrapers/cppref/c.rb
@@ -2,7 +2,7 @@ module Docs
   class C < Cppref
     self.name = 'C'
     self.slug = 'c'
-    self.base_url = 'https://en.cppreference.com/w/c/'
+    self.base_url = 'https://en.cppreference.com/w/c'
     # release = '2023-03-24'
 
     html_filters.insert_before 'cppref/clean_html', 'c/entries'
--- lib/docs/scrapers/cppref/cpp.rb
+++ lib/docs/scrapers/cppref/cpp.rb
@@ -2,7 +2,7 @@ module Docs
   class Cpp < Cppref
     self.name = 'C++'
     self.slug = 'cpp'
-    self.base_url = 'https://en.cppreference.com/w/cpp/'
+    self.base_url = 'https://en.cppreference.com/w/'
     # release = '2023-03-24'
 
     html_filters.insert_before 'cppref/clean_html', 'cpp/entries'
