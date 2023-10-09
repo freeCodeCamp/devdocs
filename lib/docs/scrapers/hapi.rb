b03af5075577fec1f0de82233fa6f6824dca702c
Simon Legner <Simon.Legner@gmail.com>
2023-10-09 20:11:45 +0200

    hapi: update attribution

--- lib/docs/scrapers/hapi.rb
+++ lib/docs/scrapers/hapi.rb
@@ -16,15 +16,9 @@ module Docs
     options[:container] = '.markdown-wrapper'
     options[:title] = "Hapi"
     options[:attribution] = <<-HTML
-      Copyright &copy; 2011-2022, Project contributors Copyright &copy; 2011-2020, Sideway Inc Copyright &copy; 2011-2014, Walmart
-      Copyright &copy; 2011, Yahoo Inc.
-      All rights reserved.
-
-      Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-      Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-      Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-      The names of any contributors may not be used to endorse or promote products derived from this software without specific prior written permission.
+      Copyright &copy; 2011-2022, Project contributors Copyright &copy; 2011-2020, Sideway Inc Copyright &copy; 2011-2014, Walmart<br>
+      Copyright &copy; 2011, Yahoo Inc.<br>
+      Licensed under the BSD 3-clause License.
     HTML
 
     def get_latest_version(opts)
