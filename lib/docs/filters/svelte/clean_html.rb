0ac54123585e40bab507cdf213bd36c214677702
Simon Legner <Simon.Legner@gmail.com>
2023-10-09 21:55:03 +0200

    Update Svelte documentation (4.2.1)

--- lib/docs/filters/svelte/clean_html.rb
+++ lib/docs/filters/svelte/clean_html.rb
@@ -2,8 +2,12 @@ module Docs
   class Svelte
     class CleanHtmlFilter < Filter
       def call
-        @doc = at_css('main > .content')
-        at_css('h1').content = 'Svelte'
+        @doc = at_css('main .page.content')
+        at_css('h1').content = 'Svelte' if root_page?
+        css('pre').each do |node|
+          node.content = node.css('.line').map(&:content).join("\n")
+          node['data-language'] = 'javascript'
+        end
         doc
       end
     end
--- lib/docs/scrapers/svelte.rb
+++ lib/docs/scrapers/svelte.rb
@@ -8,18 +8,27 @@ module Docs
       code: 'https://github.com/sveltejs/svelte'
     }
 
+    self.root_path = 'introduction'
     options[:root_title] = 'Svelte'
 
+    # https://github.com/sveltejs/svelte/blob/master/LICENSE.md
     options[:attribution] = <<-HTML
-      &copy; 2016–2022 Rich Harris and contributors<br>
+      &copy; 2016–2023 Rich Harris and contributors<br>
       Licensed under the MIT License.
     HTML
 
     options[:skip] = %w(team.html plugins/)
 
-    self.release = '3.55.0'
-    self.base_url = 'https://svelte.dev/docs'
+    self.base_url = 'https://svelte.dev/docs/'
     html_filters.push 'svelte/entries', 'svelte/clean_html'
+    
+    version do
+      self.release = '4.2.1'
+    end
+
+    version '3' do
+      self.release = '3.55.0'
+    end
 
     def get_latest_version(opts)
       get_npm_version('svelte', opts)
