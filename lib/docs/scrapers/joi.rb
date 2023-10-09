474c7b495a78ce1c65c48749dd9a3e37a34f15bc
Simon Legner <Simon.Legner@gmail.com>
2023-10-09 20:30:32 +0200

    Update Joi documentation (17.11.0)

--- lib/docs/scrapers/joi.rb
+++ lib/docs/scrapers/joi.rb
@@ -4,7 +4,7 @@ module Docs
     self.name = "Joi"
     self.slug = "joi"
     self.type = "joi"
-    self.release = "17.9.1"
+    self.release = "17.11.0"
     self.base_url = "https://joi.dev/api/?v=#{self.release}"
     self.links = {
       home: "https://joi.dev/",
@@ -16,14 +16,8 @@ module Docs
     options[:container] = '.markdown-wrapper'
     options[:title] = "Joi"
     options[:attribution] = <<-HTML
-      Copyright &copy; 2012-2022, Project contributors Copyright &copy; 2012-2022, Sideway Inc Copyright &copy; 2012-2014, Walmart
-      All rights reserved.
-
-      Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-      Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-      Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-      The names of any contributors may not be used to endorse or promote products derived from this software without specific prior written permission.
+      Copyright &copy; 2012-2022, Project contributors Copyright &copy; 2012-2022, Sideway Inc Copyright &copy; 2012-2014, Walmart<br>
+      Licensed under the BSD 3-clause License.
     HTML
 
     def get_latest_version(opts)
