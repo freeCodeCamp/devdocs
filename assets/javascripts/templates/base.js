b4cf75666997212c6e1727e5d96ddb6090720710
Simon Legner <Simon.Legner@gmail.com>
2024-01-06 16:03:21 +0100

    Sanity-check decaffeinate app.templates.render

--- assets/javascripts/templates/base.js
+++ assets/javascripts/templates/base.js
@@ -1,22 +1,14 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-/*
- * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
 app.templates.render = function (name, value, ...args) {
   const template = app.templates[name];
 
   if (Array.isArray(value)) {
     let result = "";
-    for (var val of Array.from(value)) {
-      result += template(val, ...Array.from(args));
+    for (var val of value) {
+      result += template(val, ...args);
     }
     return result;
   } else if (typeof template === "function") {
-    return template(value, ...Array.from(args));
+    return template(value, ...args);
   } else {
     return template;
   }
