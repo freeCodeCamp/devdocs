46d8d85ee288e26708c8b9224def33d17bb1d91b
Simon Legner <Simon.Legner@gmail.com>
2024-01-06 15:10:21 +0100

    Sanity-check decaffeinate app.views.RootPage

--- assets/javascripts/views/content/root_page.js
+++ assets/javascripts/views/content/root_page.js
@@ -1,15 +1,5 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS206: Consider reworking classes to avoid initClass
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
 app.views.RootPage = class RootPage extends app.View {
-  static initClass() {
-    this.events = { click: "onClick" };
-  }
+  static events = { click: "onClick" };
 
   init() {
     if (!this.isHidden()) {
@@ -54,4 +44,3 @@ app.views.RootPage = class RootPage extends app.View {
     }
   }
 };
-app.views.RootPage.initClass();
