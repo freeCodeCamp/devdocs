92b182806bb718a44d36fe02cdce9b532f173b9f
Simon Legner <Simon.Legner@gmail.com>
2024-01-06 15:31:59 +0100

    Sanity-check decaffeinate app.views.RdocPage

--- assets/javascripts/views/pages/rdoc.js
+++ assets/javascripts/views/pages/rdoc.js
@@ -1,17 +1,7 @@
-// TODO: This file was created by bulk-decaffeinate.
-// Sanity-check the conversion and remove this comment.
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS206: Consider reworking classes to avoid initClass
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
 //= require views/pages/base
 
 app.views.RdocPage = class RdocPage extends app.views.BasePage {
-  static initClass() {
-    this.events = { click: "onClick" };
-  }
+  static events = { click: "onClick" };
 
   onClick(event) {
     if (!event.target.classList.contains("method-click-advice")) {
@@ -29,4 +19,3 @@ app.views.RdocPage = class RdocPage extends app.views.BasePage {
     return (event.target.textContent = isShown ? "Show source" : "Hide source");
   }
 };
-app.views.RdocPage.initClass();
