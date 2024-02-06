903fac58c242bca8d4d928e286b0feac1cd786a8
Mike Murphy <elmiko@gmail.com>
2024-02-05 17:28:23 -0800

    Fixed compile error in nextjs/clean_html.rb

--- lib/docs/filters/nextjs/clean_html.rb
+++ lib/docs/filters/nextjs/clean_html.rb
@@ -6,9 +6,8 @@ module Docs
                 doc.prepend_child("<h1>htmx</h1>") if root_page?
                 css('div:contains("NEWS:")').remove
                 css('h2:contains("sponsors"), #sponsor-table').remove
-            end
                 doc
-        
+            end
         end
     end
 end
