731641d6dcb59c997f8f614e9b7e392312186a50
Simon Legner <Simon.Legner@gmail.com>
2024-08-18 12:28:24 +0200

    Update Prettier documentation (3.3.3)

--- lib/docs/scrapers/prettier.rb
+++ lib/docs/scrapers/prettier.rb
@@ -2,7 +2,7 @@ module Docs
   class Prettier < UrlScraper
     self.name = 'Prettier'
     self.type = 'simple'
-    self.release = '3.0.0'
+    self.release = '3.3.3'
     self.base_url = 'https://prettier.io/docs/en/'
     self.links = {
       home: 'https://prettier.io/',
