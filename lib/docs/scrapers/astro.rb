d579d97fa3b626adf96827daff3510a3c1909b90
Simon Legner <Simon.Legner@gmail.com>
2024-08-18 11:38:41 +0200

    Update Astro documentation (4.14.2)

--- lib/docs/scrapers/astro.rb
+++ lib/docs/scrapers/astro.rb
@@ -16,7 +16,7 @@ module Docs
 
     options[:skip_patterns] = [/tutorial/]
 
-    self.release = '4.12.3'
+    self.release = '4.14.2'
     self.base_url = 'https://docs.astro.build/en/'
     self.initial_paths = %w(install-and-setup/)
 
