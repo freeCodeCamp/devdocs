6e5dd89eb515fea6eec04f2859380cb67168b4c5
Simon Legner <Simon.Legner@gmail.com>
2024-08-18 11:58:26 +0200

    git: fix license

--- lib/docs/scrapers/git.rb
+++ lib/docs/scrapers/git.rb
@@ -15,9 +15,10 @@ module Docs
     options[:only_patterns] = [/\A\/[^\/]+\z/]
     options[:skip] = %w(/howto-index.html)
 
+    # https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt
     options[:attribution] = <<-HTML
-      &copy; 2005&ndash;2024 Linus Torvalds and others<br>
-      Licensed under the GNU General Public License version 2.
+      &copy; 2012&ndash;2024 Scott Chacon and others<br>
+      Licensed under the MIT License.
     HTML
 
     def get_latest_version(opts)
