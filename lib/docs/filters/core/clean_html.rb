e68269b3f4ce4a0ae896d93be35af8cc9472d609
Tim Lim <tim@thewheatfield.org>
2024-10-13 19:19:48 +0800

    Fix Cordova indents with new option

--- lib/docs/filters/core/clean_html.rb
+++ lib/docs/filters/core/clean_html.rb
@@ -8,6 +8,7 @@ module Docs
       xpath('./text()', './/text()[not(ancestor::pre) and not(ancestor::code) and not(ancestor::div[contains(concat(" ", normalize-space(@class), " "), " prism ")])]').each do |node|
         content = node.content
         next unless content.valid_encoding?
+        next if context[:clean_html_maintain_nbsp] && !content.index(" ").nil?
         content.gsub! %r{[[:space:]]+}, ' '
         node.content = content
       end
