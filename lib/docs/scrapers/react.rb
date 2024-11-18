7c5132afecf42b916aa6b0c9aba03e7da21e2464
Gergely Gombos <gombosg@disroot.org>
2024-11-18 08:06:35 +0100

    Separate React 17

--- lib/docs/scrapers/react.rb
+++ lib/docs/scrapers/react.rb
@@ -2,37 +2,40 @@ module Docs
   class React < UrlScraper
     self.name = 'React'
     self.type = 'simple'
-    self.release = '18.2.0'
-    self.base_url = 'https://reactjs.org/docs/'
-    self.root_path = 'hello-world.html'
     self.links = {
-      home: 'https://reactjs.org/',
+      home: 'https://react.dev/',
       code: 'https://github.com/facebook/react'
     }
 
-    html_filters.push 'react/entries', 'react/clean_html'
+    version '17' do
+      self.release = '17.0.2'
+      self.base_url = 'https://17.reactjs.org/docs/'
+      self.root_path = 'hello-world.html'
+      html_filters.push 'react/entries', 'react/clean_html'
 
-    options[:skip] = %w(
-      codebase-overview.html
-      design-principles.html
-      how-to-contribute.html
-      implementation-notes.html
-    )
+      options[:skip] = %w(
+        codebase-overview.html
+        design-principles.html
+        how-to-contribute.html
+        implementation-notes.html
+      )
 
-    options[:replace_paths] = {
-      'more-about-refs.html' => 'refs-and-the-dom.html',
-      'interactivity-and-dynamic-uis.html' => 'state-and-lifecycle.html',
-      'working-with-the-browser.html' => 'refs-and-the-dom.html',
-      'top-level-api.html' => 'react-api.html',
-    }
+      options[:replace_paths] = {
+        'more-about-refs.html' => 'refs-and-the-dom.html',
+        'interactivity-and-dynamic-uis.html' => 'state-and-lifecycle.html',
+        'working-with-the-browser.html' => 'refs-and-the-dom.html',
+        'top-level-api.html' => 'react-api.html',
+      }
 
-    options[:attribution] = <<-HTML
-      &copy; 2013&ndash;present Facebook Inc.<br>
-      Licensed under the Creative Commons Attribution 4.0 International Public License.
-    HTML
+      options[:attribution] = <<-HTML
+        &copy; 2013&ndash;present Facebook Inc.<br>
+        Licensed under the Creative Commons Attribution 4.0 International Public License.
+      HTML
+    end
+
 
     def get_latest_version(opts)
-      doc = fetch_doc('https://reactjs.org/docs/getting-started.html', opts)
+      doc = fetch_doc('https://react.dev/', opts)
       doc.at_css('a[href="/versions"]').content.strip[1..-1]
     end
   end
