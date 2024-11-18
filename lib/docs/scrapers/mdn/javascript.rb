a5af2487ec1298fa3ed14167d9df9e60e3b3e108
Simon Legner <Simon.Legner@gmail.com>
2024-11-18 18:00:04 +0100

    Update JavaScript documentation

--- lib/docs/scrapers/mdn/javascript.rb
+++ lib/docs/scrapers/mdn/javascript.rb
@@ -3,7 +3,7 @@ module Docs
     prepend FixInternalUrlsBehavior
     prepend FixRedirectionsBehavior
 
-    # release = '2024-08-20'
+    # release = '2024-11-18'
     self.name = 'JavaScript'
     self.base_url = 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference'
     self.links = {
