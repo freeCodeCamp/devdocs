df398bd145f43dc953d751c4f722924930ec1c0f
Gergely Gombos <gombosg@disroot.org>
2024-12-01 20:51:49 +0100

    add categories

--- lib/docs/filters/react/entries_react_dev.rb
+++ lib/docs/filters/react/entries_react_dev.rb
@@ -4,11 +4,12 @@ module Docs
       def get_name
         canary_copy = '- This feature is available in the latest Canary'
         name = at_css('article h1').content
-        return name.sub(canary_copy, ' (experimental)')
+        return name.sub(canary_copy, ' (Experimental)')
       end
 
       def get_type
-        return 'TODO add types'
+        breadcrumb_nodes = css('a.tracking-wide')
+        return breadcrumb_nodes.last.content || 'Miscellaneous'
       end
     end
   end
