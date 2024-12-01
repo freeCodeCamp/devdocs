30ddc7bbaf106320090def89f40f952be7733574
Gergely Gombos <gombosg@disroot.org>
2024-12-01 20:36:04 +0100

    sanitize titles - experimental tag

--- lib/docs/filters/react/entries_react_dev.rb
+++ lib/docs/filters/react/entries_react_dev.rb
@@ -2,7 +2,9 @@ module Docs
   class React
     class EntriesReactDevFilter < Docs::EntriesFilter
       def get_name
-        at_css('article h1').content
+        canary_copy = '- This feature is available in the latest Canary'
+        name = at_css('article h1').content
+        return name.sub(canary_copy, ' (experimental)')
       end
 
       def get_type
