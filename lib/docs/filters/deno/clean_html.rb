de294da7edc1def1beb0ce200e8034f11ded42c2
Simon Legner <Simon.Legner@gmail.com>
2024-12-07 16:16:33 +0100

    Update Deno documentation (2.1.1)

--- lib/docs/filters/deno/clean_html.rb
+++ lib/docs/filters/deno/clean_html.rb
@@ -8,12 +8,20 @@ module Docs
           @doc = at_css('main article .markdown-body')
         end
 
+        if at_css('.text-2xl')
+          doc.prepend_child at_css('.text-2xl').remove
+          at_css('.text-2xl').name = 'h1'
+        end
+
         css('code').each do |node|
           if node['class']
             lang = node['class'][/language-(\w+)/, 1]
           end
           node['data-language'] = lang || 'ts'
           node.remove_attribute('class')
+          if node.parent.name == 'div'
+            node.content = node.content.strip
+          end
         end
 
         css('a.header-anchor').remove()
