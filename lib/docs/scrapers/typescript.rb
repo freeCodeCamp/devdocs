1f9cce1a4aed350f5b39df780281dd610b2a5783
Simon Legner <Simon.Legner@gmail.com>
2024-12-07 16:20:07 +0100

    Update TypeScript documentation (5.7.2)

--- lib/docs/scrapers/typescript.rb
+++ lib/docs/scrapers/typescript.rb
@@ -45,7 +45,7 @@ module Docs
     HTML
 
     version do
-      self.release = '5.6.3'
+      self.release = '5.7.2'
       self.base_urls = [
         'https://www.typescriptlang.org/docs/handbook/',
         'https://www.typescriptlang.org/'
