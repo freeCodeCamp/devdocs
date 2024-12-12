7d2643260d1b9623624e454e8ae70603602230a3
Simon Legner <Simon.Legner@gmail.com>
2024-12-12 08:53:09 +0100

    Update React documentation (19)

--- lib/docs/scrapers/react.rb
+++ lib/docs/scrapers/react.rb
@@ -8,19 +8,27 @@ module Docs
       code: 'https://github.com/facebook/react'
     }
 
+    options[:attribution] = <<-HTML
+      &copy; 2013&ndash;present Facebook Inc.<br>
+      Licensed under the Creative Commons Attribution 4.0 International Public License.
+    HTML
+
     version do
-      self.release = '18.3.1'
+      self.release = '19'
       self.base_url = 'https://react.dev'
       self.initial_paths = %w(/reference/react /learn)
-
       html_filters.push 'react/entries_react_dev', 'react/clean_html_react_dev'
 
       options[:only_patterns] = [/\A\/learn/, /\A\/reference/]
+    end
 
-      options[:attribution] = <<-HTML
-        &copy; 2013&ndash;present Facebook Inc.<br>
-        Licensed under the Creative Commons Attribution 4.0 International Public License.
-      HTML
+    version '18' do
+      self.release = '18.3.1'
+      self.base_url = 'https://18.react.dev'
+      self.initial_paths = %w(/reference/react /learn)
+      html_filters.push 'react/entries_react_dev', 'react/clean_html_react_dev'
+
+      options[:only_patterns] = [/\A\/learn/, /\A\/reference/]
     end
 
     version '17' do
@@ -42,11 +50,6 @@ module Docs
         'working-with-the-browser.html' => 'refs-and-the-dom.html',
         'top-level-api.html' => 'react-api.html',
       }
-
-      options[:attribution] = <<-HTML
-        &copy; 2013&ndash;present Facebook Inc.<br>
-        Licensed under the Creative Commons Attribution 4.0 International Public License.
-      HTML
     end
 
     def get_latest_version(opts)
