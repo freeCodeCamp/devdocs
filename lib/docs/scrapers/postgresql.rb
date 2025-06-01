896f8e0a023f6481e51d1845291ae86aa3a10296
Simon Legner <Simon.Legner@gmail.com>
2025-06-01 12:25:54 +0200

    Update PostgreSQL documentation (17.5)

--- lib/docs/scrapers/postgresql.rb
+++ lib/docs/scrapers/postgresql.rb
@@ -17,6 +17,7 @@ module Docs
     options[:title] = false
     options[:root_title] = 'PostgreSQL'
     options[:follow_links] = ->(filter) { filter.initial_page? }
+    options[:rate_limit] = 200
 
     options[:skip] = %w(
       index.html
@@ -51,12 +52,12 @@ module Docs
       /\Aunsupported-features/ ]
 
     options[:attribution] = <<-HTML
-      &copy; 1996&ndash;2024 The PostgreSQL Global Development Group<br>
+      &copy; 1996&ndash;2025 The PostgreSQL Global Development Group<br>
       Licensed under the PostgreSQL License.
     HTML
 
     version '17' do
-      self.release = '17.1'
+      self.release = '17.5'
       self.base_url = "https://www.postgresql.org/docs/#{version}/"
     end
 
