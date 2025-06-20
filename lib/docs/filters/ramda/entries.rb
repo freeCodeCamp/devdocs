2b035804da5a215af86da454e38a49e2e1fdcc1b
Alex Bepple <alex@bepple.de>
2025-06-20 19:00:59 +0200

    (ramda) $! make docs generatable again

--- lib/docs/filters/ramda/entries.rb
+++ lib/docs/filters/ramda/entries.rb
@@ -3,7 +3,9 @@ module Docs
     class EntriesFilter < Docs::EntriesFilter
       def additional_entries
         css('ul.toc li').map do |node|
-          ["R.#{node['data-name']}", node['data-name'], node['data-category'].sub('Relaction', 'Relation')]
+          # As of 2025-06-20, `data-category` attribute is missing on https://ramdajs.com/ for Ramda versions < 0.29.0.
+          # This results in missing type for entries – and docs cannot be generated.
+          ["R.#{node['data-name']}", node['data-name'], node['data-category']]
         end
       end
     end
