9117c2d2d3521d23c10b6840a44552c70164d810
Simon Legner <Simon.Legner@gmail.com>
2025-07-01 18:51:03 +0200

    Update Git documentation (2.50.0)

--- lib/docs/scrapers/git.rb
+++ lib/docs/scrapers/git.rb
@@ -1,9 +1,37 @@
 module Docs
   class Git < UrlScraper
     self.type = 'git'
-    self.release = '2.48.1'
+    self.release = '2.50.0'
     self.base_url = 'https://git-scm.com/docs'
-    self.initial_paths = %w(/git.html)
+    self.initial_paths = %w(
+      /git.html
+      /git-archimport.html
+      /git-cherry.html
+      /git-citool.html
+      /git-column.html
+      /git-cvsexportcommit.html
+      /git-for-each-repo.html
+      /git-get-tar-commit-id.html
+      /git-http-fetch.html
+      /git-http-push.html
+      /git-merge-file.html
+      /git-merge-index.html
+      /git-merge-one-file.html
+      /git-merge-tree.html
+      /git-mktree.html
+      /git-p4.html
+      /git-pack-redundant.html
+      /git-quiltimport.html
+      /git-replay.html
+      /git-sh-i18n.html
+      /git-sh-i18n--envsubst.html
+      /git-sh-setup.html
+      /git-show-index.html
+      /git-unpack-file.html
+      /git-verify-commit.html
+      /gitformat-index.html
+      /scalar.html
+    )
     self.links = {
       home: 'https://git-scm.com/',
       code: 'https://github.com/git/git'
@@ -16,9 +44,9 @@ module Docs
     options[:skip] = %w(/howto-index.html)
 
     # https://github.com/git/git?tab=License-1-ov-file#readme
-    # NOT https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt
+    # NOT https://github.com/git/git-scm.com/blob/gh-pages/MIT-LICENSE.txt
     options[:attribution] = <<-HTML
-      &copy; 2005&ndash;2024 Linus Torvalds and others<br>
+      &copy; 2005&ndash;2025 Linus Torvalds and others<br>
       Licensed under the GNU General Public License version 2.
     HTML
 
