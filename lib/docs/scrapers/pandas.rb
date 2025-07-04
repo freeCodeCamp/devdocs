06ce9ee6793faeef3746e2e40a7856b7ba866b94
Simon Legner <Simon.Legner@gmail.com>
2025-07-04 10:35:40 +0200

    Update pandas documentation (2.3.0)

--- lib/docs/scrapers/pandas.rb
+++ lib/docs/scrapers/pandas.rb
@@ -11,13 +11,15 @@ module Docs
     options[:skip] = %w(internals.html release.html contributing.html whatsnew.html)
     options[:skip_patterns] = [/whatsnew\//]
 
+    # https://github.com/pandas-dev/pandas/blob/main/LICENSE
     options[:attribution] = <<-HTML
-      &copy; 2008&ndash;2022, AQR Capital Management, LLC, Lambda Foundry, Inc. and PyData Development Team<br>
+      &copy; 2008&ndash;2011, AQR Capital Management, LLC, Lambda Foundry, Inc. and PyData Development Team<br>
+      &copy; 2011&ndash;2025, Open source contributors<br>
       Licensed under the 3-clause BSD License.
     HTML
 
     version '2' do
-      self.release = '2.2.2'
+      self.release = '2.3.0'
       self.base_url = "https://pandas.pydata.org/pandas-docs/version/#{self.release}/"
 
       html_filters.push 'pandas/clean_html', 'pandas/entries'
