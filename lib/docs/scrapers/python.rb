47913a4e4fcd7ba4f5ade55ef7103fa51f5fc3df
Simon Legner <Simon.Legner@gmail.com>
2025-08-14 18:32:07 +0200

    Update Python documentation (3.14.0rc2)

--- lib/docs/scrapers/python.rb
+++ lib/docs/scrapers/python.rb
@@ -27,6 +27,13 @@ module Docs
       Licensed under the PSF License.
     HTML
 
+    version '3.14' do
+      self.release = '3.14.0rc2'
+      self.base_url = "https://docs.python.org/#{self.version}/"
+
+      html_filters.push 'python/entries_v3', 'sphinx/clean_html', 'python/clean_html'
+    end
+
     version '3.13' do
       self.release = '3.13.6'
       self.base_url = "https://docs.python.org/#{self.version}/"
