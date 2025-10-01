5228d1d3802f187e0690f9488ccc04d89ef403c2
spamguy <spamguy@spamguy.org>
2025-10-01 11:26:11 -0700

    Bump homebrew to 4.6.15

--- lib/docs/scrapers/homebrew.rb
+++ lib/docs/scrapers/homebrew.rb
@@ -2,7 +2,7 @@ module Docs
   class Homebrew < UrlScraper
     self.name = 'Homebrew'
     self.type = 'simple'
-    self.release = '3.5.10'
+    self.release = '4.6.15'
     self.base_url = 'https://docs.brew.sh/'
     self.links = {
       home: 'https://brew.sh',
