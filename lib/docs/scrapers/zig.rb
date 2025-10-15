4a23f5e3d6200354806e8f7b8b8cb9a2502e1fa9
Simon Legner <Simon.Legner@gmail.com>
2025-10-15 17:24:26 +0200

    Update Zig documentation (0.15.2)

--- lib/docs/scrapers/zig.rb
+++ lib/docs/scrapers/zig.rb
@@ -2,7 +2,7 @@ module Docs
   class Zig < UrlScraper
     self.name = 'Zig'
     self.type = 'simple'
-    self.release = '0.14.0'
+    self.release = '0.15.2'
     self.base_url = "https://ziglang.org/documentation/#{self.release}/"
     self.links = {
       home: 'https://ziglang.org/',
