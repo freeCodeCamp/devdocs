2a24012c6e23ffdba2ad630b585ba0332b575267
Simon Legner <Simon.Legner@gmail.com>
2026-02-14 11:25:18 +0100

    Update htmx documentation (2.0.7)

--- lib/docs/scrapers/htmx.rb
+++ lib/docs/scrapers/htmx.rb
@@ -7,26 +7,34 @@ module Docs
       home: 'https://htmx.org/',
       code: 'https://github.com/bigskysoftware/htmx'
     }
-	self.release = '1.9.10'
-	self.base_url = "https://htmx.org/"
     self.initial_paths = %w(reference/)
 
     html_filters.push 'htmx/entries', 'htmx/clean_html'
 
     options[:trailing_slash] = true
-	options[:container] = '.content'
+    options[:container] = '.content'
     options[:download_images] = false
     options[:skip_patterns] = [
-		/\Aessays/,
-		/\Aexamples/,
-		/\Amigration-guide/,
-		/\Aposts/,
-	]
+      /\Aessays/,
+      /\Aexamples/,
+      /\Amigration-guide/,
+      /\Aposts/,
+    ]
 
-	# https://github.com/bigskysoftware/htmx/blob/master/LICENSE
+    # https://github.com/bigskysoftware/htmx/blob/master/LICENSE
     options[:attribution] = <<-HTML
-	  Licensed under the Zero-Clause BSD License.
+    Licensed under the Zero-Clause BSD License.
     HTML
+
+    version do
+      self.release = '2.0.7'
+      self.base_url = "https://htmx.org/"
+    end
+
+    version '1' do
+      self.release = '1.9.12'
+      self.base_url = "https://v1.htmx.org/"
+    end
 
     def get_latest_version(opts)
       get_npm_version('htmx.org', opts)
