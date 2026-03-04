253d7262c23b488c500064287746b009e36dc959
Simon Legner <Simon.Legner@gmail.com>
2026-03-04 20:52:57 +0100

    Update Fish documentation (4.5.0)

--- lib/docs/scrapers/fish.rb
+++ lib/docs/scrapers/fish.rb
@@ -16,6 +16,14 @@ module Docs
       Licensed under the GNU General Public License, version 2.
     HTML
 
+    version '4.5' do
+      self.release = '4.5.0'
+      self.base_url = "https://fishshell.com/docs/#{version}/"
+
+      options[:skip].concat %w(genindex.html relnotes.html)
+      html_filters.push 'sphinx/clean_html', 'fish/clean_html_sphinx', 'fish/entries_sphinx'
+    end
+
     version '4.4' do
       self.release = '4.4.0'
       self.base_url = "https://fishshell.com/docs/#{version}/"
