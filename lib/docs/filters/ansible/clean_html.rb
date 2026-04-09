725360b9b8b5118c7f44ce2b516a242f220a73e3
Vinuthna <vinuthnainti28@gmail.com>
2026-04-09 18:08:17 +0530

    Fix: add YAML syntax highlighting for Ansible docs

--- lib/docs/filters/ansible/clean_html.rb
+++ lib/docs/filters/ansible/clean_html.rb
@@ -13,7 +13,10 @@ module Docs
             subnode.remove_attribute('style')
           end
         end
-
+        css('pre > code').each do |node|
+        existing_class = node['class']
+        node['class'] = [existing_class, 'language-yaml'].compact.join(' ')
+        end
         doc
 
       end
